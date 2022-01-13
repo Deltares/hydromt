6273f7840d29102a62359b63e80682343ea21e90
DirkEilander <dirk.eilander@gmail.com>
2022-01-13 13:37:10 +0100

    remove unused inports and misplaced warning filter in stats/skill

--- hydromt/stats/skills.py
+++ hydromt/stats/skills.py
@@ -3,16 +3,10 @@
 
 import xarray as xr
 import numpy as np
-import pandas as pd
-import calendar
-from datetime import timedelta, datetime
 import bottleneck
-import warnings
 
-warnings.filterwarnings("ignore")
 
-# PERFORMANCE METRICS
-def bias(sim, obs, dim="time"):
+def bias(sim: xr.DataArray, obs: xr.DataArray, dim: str = "time") -> xr.DataArray:
     """Returns the bias between two time series.
 
         .. math::
@@ -20,16 +14,16 @@ def bias(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         bias
     """
     # wrap numpy function
@@ -41,7 +35,9 @@ def bias(sim, obs, dim="time"):
     return bias
 
 
-def percentual_bias(sim, obs, dim="time"):
+def percentual_bias(
+    sim: xr.DataArray, obs: xr.DataArray, dim: str = "time"
+) -> xr.DataArray:
     """Returns the percentual bias between two time series.
 
         .. math::
@@ -49,16 +45,16 @@ def percentual_bias(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         percentual bias
     """
     # wrap numpy function
@@ -70,7 +66,9 @@ def percentual_bias(sim, obs, dim="time"):
     return pbias
 
 
-def nashsutcliffe(sim, obs, dim="time"):
+def nashsutcliffe(
+    sim: xr.DataArray, obs: xr.DataArray, dim: str = "time"
+) -> xr.DataArray:
     """Returns the Nash-Sutcliffe model efficiency based on a simulated
     and observed time series.
 
@@ -79,16 +77,16 @@ def nashsutcliffe(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the Nash-Sutcliffe model efficiency
     """
     # wrap numpy function
@@ -100,7 +98,9 @@ def nashsutcliffe(sim, obs, dim="time"):
     return nse
 
 
-def lognashsutcliffe(sim, obs, epsilon=1e-6, dim="time"):
+def lognashsutcliffe(
+    sim: xr.DataArray, obs: xr.DataArray, epsilon: float = 1e-6, dim: str = "time"
+) -> xr.DataArray:
     """Returns the log Nash-Sutcliffe model efficiency based on simulated
     and observed time series.
 
@@ -109,9 +109,9 @@ def lognashsutcliffe(sim, obs, epsilon=1e-6, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     epsilon : float, optional
         small value to avoid taking the log of zero (the default is 1e-6)
@@ -120,7 +120,7 @@ def lognashsutcliffe(sim, obs, epsilon=1e-6, dim="time"):
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the log of the Nash-Sutcliffe model efficiency
     """
     obs = np.log(obs + epsilon)
@@ -134,21 +134,23 @@ def lognashsutcliffe(sim, obs, epsilon=1e-6, dim="time"):
     return log_nse
 
 
-def pearson_correlation(sim, obs, dim="time"):
+def pearson_correlation(
+    sim: xr.DataArray, obs: xr.DataArray, dim: str = "time"
+) -> xr.DataArray:
     """Returns the Pearson correlation coefficient of two time series.
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the pearson correlation coefficient
     """
     # wrap numpy function
@@ -160,22 +162,24 @@ def pearson_correlation(sim, obs, dim="time"):
     return pearsonr
 
 
-def spearman_rank_correlation(sim, obs, dim="time"):
+def spearman_rank_correlation(
+    sim: xr.DataArray, obs: xr.DataArray, dim: str = "time"
+) -> xr.DataArray:
     """Returns the spearman rank correlation coefficient of
     two time series.
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the spearman rank correlation
     """
     # wrap numpy function
@@ -187,8 +191,11 @@ def spearman_rank_correlation(sim, obs, dim="time"):
     return spearmanr
 
 
-def kge_non_parametric(sim, obs, dim="time"):
-    """Returns the Non Parametric Kling-Gupta Efficiency (KGE, 2018) of two time series with decomposed scores
+def kge_non_parametric(
+    sim: xr.DataArray, obs: xr.DataArray, dim: str = "time"
+) -> xr.Dataset:
+    """Returns the Non Parametric Kling-Gupta Efficiency (KGE) of two
+    time series with decomposed scores (Pool et al., 2018)
 
     .. ref::
     Pool, Vis, and Seibert, 2018 Evaluating model performance: towards
@@ -196,17 +203,17 @@ def kge_non_parametric(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataSet
-        Non Parametric Kling-Gupta Efficiency (2018) and with decomposed score
+    xarray.DataSet
+        Non Parametric KGE and decomposed score
     """
     cc = spearman_rank_correlation(sim, obs, dim=dim)
     cc.name = "kge_np_spearman_rank_correlation_coef"
@@ -223,8 +230,11 @@ def kge_non_parametric(sim, obs, dim="time"):
     return ds_out
 
 
-def kge_non_parametric_flood(sim, obs, dim="time"):
-    """Returns the Non Parametric Kling-Gupta Efficiency (KGE, 2018) of two time series optimized for flood peaks using Pearson (see Pool et al., 2018)
+def kge_non_parametric_flood(
+    sim: xr.DataArray, obs: xr.DataArray, dim: str = "time"
+) -> xr.Dataset:
+    """Returns the Non Parametric Kling-Gupta Efficiency (KGE) of two time
+    series optimized for flood peaks using Pearson correlation (Pool et al., 2018)
 
     .. ref::
     Pool, Vis, and Seibert, 2018 Evaluating model performance: towards
@@ -232,18 +242,17 @@ def kge_non_parametric_flood(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataSet
-        Non Parametric Kling-Gupta Efficiency (2018) optimize for flood peaks
-        using pearson (see Pool et al., 2018) and with decomposed score
+    xarray.DataSet
+        Non Parametric KGE optimize for flood peaks and decomposed scores
     """
     # cc = spearman_rank_correlation(sim, obs)
     cc = pearson_correlation(sim, obs, dim=dim)
@@ -261,7 +270,7 @@ def kge_non_parametric_flood(sim, obs, dim="time"):
     return ds_out
 
 
-def rsquared(sim, obs, dim="time"):
+def rsquared(sim: xr.DataArray, obs: xr.DataArray, dim: str = "time") -> xr.DataArray:
     """Returns the coefficient of determination of two time series.
 
         .. math::
@@ -269,16 +278,16 @@ def rsquared(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the coefficient of determination
     """
     # R2 equals the square of the Pearson correlation coefficient between obs and sim
@@ -287,7 +296,7 @@ def rsquared(sim, obs, dim="time"):
     return rsquared
 
 
-def mse(sim, obs, dim="time"):
+def mse(sim: xr.DataArray, obs: xr.DataArray, dim: str = "time") -> xr.DataArray:
     """Returns the mean squared error (MSE) between two time series.
 
         .. math::
@@ -295,16 +304,16 @@ def mse(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the mean squared error
     """
     # wrap numpy function
@@ -316,7 +325,7 @@ def mse(sim, obs, dim="time"):
     return mse
 
 
-def rmse(sim, obs, dim="time"):
+def rmse(sim: xr.DataArray, obs: xr.DataArray, dim: str = "time") -> xr.DataArray:
     """Returns the root mean squared error between two time series.
 
         .. math::
@@ -324,16 +333,16 @@ def rmse(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataArray
+    xarray.DataArray
         the root mean squared error
     """
     rmse = np.sqrt(mse(sim, obs, dim=dim))
@@ -341,8 +350,8 @@ def rmse(sim, obs, dim="time"):
     return rmse
 
 
-def kge(sim, obs, dim="time"):
-    """Returns the Kling-Gupta Efficiency (KGE) of two time series
+def kge(sim: xr.DataArray, obs: xr.DataArray, dim: str = "time") -> xr.Dataset:
+    """Returns the Kling-Gupta Efficiency (KGE) of two time series (Gupta et al. 2009)
 
         .. ref::
         Gupta, Kling, Yilmaz, Martinez, 2009, Decomposition of the mean
@@ -351,17 +360,17 @@ def kge(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataSet
-        Kling-Gupta Efficiency and with decomposed scores
+    xarray.DataSet
+        KGE with decomposed scores
     """
     cc = pearson_correlation(sim, obs, dim=dim)
     cc.name = "kge_pearson_coef"
@@ -375,8 +384,9 @@ def kge(sim, obs, dim="time"):
     return ds_out
 
 
-def kge_2012(sim, obs, dim="time"):
-    """Returns the Kling-Gupta Efficiency (KGE, 2012) of two time series
+def kge_2012(sim: xr.DataArray, obs: xr.DataArray, dim: str = "time") -> xr.Dataset:
+    """Returns the Kling-Gupta Efficiency (KGE) of two time series based on
+    Kling et al. (2012)
 
         .. ref::
         Kling, H., Fuchs, M., & Paulin, M. (2012). Runoff conditions in the
@@ -385,17 +395,17 @@ def kge_2012(sim, obs, dim="time"):
 
     Parameters
     ----------
-    sim : xarray DataArray
+    sim : xarray.DataArray
         simulations time series
-    obs : xarray DataArray
+    obs : xarray.DataArray
         observations time series
     dim : str, optional
         name of time dimension in sim and obs (the default is 'time')
 
     Returns
     -------
-    xarray DataSet
-        Kling-Gupta Efficiency (2012) and with decomposed scores
+    xarray.DataSet
+        KGE (2012) with decomposed scores
     """
     cc = pearson_correlation(sim, obs, dim=dim)
     cc.name = "kge_2012_pearson_coef"
@@ -414,7 +424,7 @@ def kge_2012(sim, obs, dim="time"):
 
 # correlation ufunc function
 # from http://xarray.pydata.org/en/stable/dask.html#automatic-parallelization
-def _covariance(x, y):
+def _covariance(x: np.ndarray, y: np.ndarray) -> float:
     return np.nanmean(
         (x - np.nanmean(x, axis=-1, keepdims=True))
         * (y - np.nanmean(y, axis=-1, keepdims=True)),
@@ -422,40 +432,40 @@ def _covariance(x, y):
     )
 
 
-def _pearson_correlation(x, y):
+def _pearson_correlation(x: np.ndarray, y: np.ndarray) -> float:
     return _covariance(x, y) / (np.nanstd(x, axis=-1) * np.nanstd(y, axis=-1))
 
 
-def _spearman_correlation(x, y):
+def _spearman_correlation(x: np.ndarray, y: np.ndarray) -> float:
     x_ranks = bottleneck.nanrankdata(x, axis=-1)
     y_ranks = bottleneck.nanrankdata(y, axis=-1)
     return _pearson_correlation(x_ranks, y_ranks)
 
 
 # numpy functions
-def _fdc_alpha(sim, obs, axis=-1):
+def _fdc_alpha(sim: np.ndarray, obs: np.ndarray, axis: int = -1) -> float:
     fdc_s = np.sort(sim, axis=axis) / (np.nanmean(sim, axis=axis) * len(sim))
     fdc_o = np.sort(obs, axis=axis) / (np.nanmean(obs, axis=axis) * len(obs))
     return 1 - 0.5 * np.nanmean(np.abs(fdc_s - fdc_o))
 
 
-def _bias(sim, obs, axis=-1):
+def _bias(sim: np.ndarray, obs: np.ndarray, axis: int = -1) -> float:
     """bias"""
     return np.nansum(sim - obs, axis=axis) / np.nansum(np.isfinite(obs), axis=axis)
 
 
-def _pbias(sim, obs, axis=-1):
+def _pbias(sim: np.ndarray, obs: np.ndarray, axis: int = -1) -> float:
     """percentual bias"""
     return np.nansum(sim - obs, axis=axis) / np.nansum(obs, axis=axis)
 
 
-def _mse(sim, obs, axis=-1):
+def _mse(sim: np.ndarray, obs: np.ndarray, axis: int = -1) -> float:
     """mean squared error"""
     mse = np.nansum((obs - sim) ** 2, axis=axis)
     return mse
 
 
-def _nse(sim, obs, axis=-1):
+def _nse(sim: np.ndarray, obs: np.ndarray, axis: int = -1) -> float:
     """nash-sutcliffe efficiency"""
     obs_mean = np.nanmean(obs, axis=axis)
     a = np.nansum((sim - obs) ** 2, axis=axis)
