f22678cc1c2fb1fcda503050cc0f94e63c20fcdc
Indra Marth <Indra.Marth@deltares.nl>
2022-01-28 16:36:05 +0100

    Add todo to change theme

--- docs/conf.py
+++ docs/conf.py
@@ -144,6 +144,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+# todo: change theme 
 #
 html_theme = "sphinx_rtd_theme"
 autodoc_member_order = "bysource"  # overwrite default alphabetical sort
