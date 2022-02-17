fdda78405543331fc69ed7fbb40da210d81caa46
Indra Marth <Indra.Marth@deltares.nl>
2022-02-17 17:31:53 +0100

    run black for cf_utils.py

--- hydromt/cf_utils.py
+++ hydromt/cf_utils.py
@@ -7,10 +7,12 @@ import shutil
 logger = logging.getLogger("Utils")
 logger.setLevel(logging.INFO)
 
-class Utils():
 
+class Utils:
     @staticmethod
-    def get_logger(log_dir_path: str, log_name: str, log_level: logging.Logger = logging.INFO):
+    def get_logger(
+        log_dir_path: str, log_name: str, log_level: logging.Logger = logging.INFO
+    ):
         """
         Creates log file
         :param log_dir_path: path where log file is written
@@ -23,19 +25,23 @@ class Utils():
         log_path = os.path.join(log_dir_path, "{}_log.txt".format(log_name))
         if os.path.exists(log_path):
             os.remove(os.path.join(log_dir_path, "{}_log.txt".format(log_name)))
-        formatter = logging.Formatter('%(name)s - %(asctime)s %(levelname)s %(message)s')
+        formatter = logging.Formatter(
+            "%(name)s - %(asctime)s %(levelname)s %(message)s"
+        )
         ch = logging.StreamHandler()
         ch.setLevel(log_level)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
-        ch = logging.FileHandler(os.path.join(log_dir_path, log_name + '.log'), mode='w')
+        ch = logging.FileHandler(
+            os.path.join(log_dir_path, log_name + ".log"), mode="w"
+        )
         ch.setLevel(log_level)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
         return logger
 
     @staticmethod
-    def get_dir(base: str, subfolders: List[str]=[]) -> Path:
+    def get_dir(base: str, subfolders: List[str] = []) -> Path:
         """
         Static methods returns WindowsPath-object of directory as listed in configuration, if needed supplemented with sub-folders.
         Checks for folder existence and creates new if needed
@@ -65,17 +71,22 @@ class Utils():
         sub_dirs = [x for x in basedir.iterdir() if x.is_dir()]
         for sub_dir in sub_dirs:
             subsub_dirs = [x for x in sub_dir.iterdir() if x.is_dir()]
-            if len(subsub_dirs)> 0:
+            if len(subsub_dirs) > 0:
                 for subsub in subsub_dirs:
                     for file in subsub.iterdir():
                         file.unlink()
                     subsub.rmdir()
             for file in sub_dir.iterdir():
-              file.unlink()
+                file.unlink()
             sub_dir.rmdir()
 
     @staticmethod
-    def create_zipfile(outfile: Path, files_to_append=List[str], add_dir_name:str=None, conditionalsuffix_sub_dir:Dict={}):
+    def create_zipfile(
+        outfile: Path,
+        files_to_append=List[str],
+        add_dir_name: str = None,
+        conditionalsuffix_sub_dir: Dict = {},
+    ):
         """
         Creates zipfile-object by adding files listed and write do destination path
         :param outfile: path to output file
@@ -85,19 +96,30 @@ class Utils():
         only applied in combination with add_dir_name
         :return:
         """
-        zipObj = zipfile.ZipFile(outfile, 'w', zipfile.ZIP_DEFLATED)
+        zipObj = zipfile.ZipFile(outfile, "w", zipfile.ZIP_DEFLATED)
         for fileObj in files_to_append:
             if add_dir_name is None:
                 zipObj.write(filename=fileObj, arcname=fileObj.name)
             if len(conditionalsuffix_sub_dir) > 0:
                 for fsuffix in conditionalsuffix_sub_dir:
                     if fileObj.suffix == fsuffix:
-                        zipObj.write(filename=fileObj, arcname='{}/{}/{}'
-                                     .format(add_dir_name, conditionalsuffix_sub_dir[fsuffix], fileObj.name))
+                        zipObj.write(
+                            filename=fileObj,
+                            arcname="{}/{}/{}".format(
+                                add_dir_name,
+                                conditionalsuffix_sub_dir[fsuffix],
+                                fileObj.name,
+                            ),
+                        )
                     else:
-                        zipObj.write(filename=fileObj, arcname='{}/{}'.format(add_dir_name, fileObj.name))
+                        zipObj.write(
+                            filename=fileObj,
+                            arcname="{}/{}".format(add_dir_name, fileObj.name),
+                        )
             else:
-                zipObj.write(filename=fileObj, arcname='{}/{}'.format(add_dir_name,fileObj.name))
+                zipObj.write(
+                    filename=fileObj, arcname="{}/{}".format(add_dir_name, fileObj.name)
+                )
         zipObj.close()
 
     @staticmethod
@@ -111,9 +133,9 @@ class Utils():
         """
         with open(ini_file, "w") as outf:
             for section in sections:
-                outf.write('[' +  section + ']\n')
+                outf.write("[" + section + "]\n")
                 for key in sections[section].keys():
-                    outf.write(key + '=' + sections[section][key] + '\n')
+                    outf.write(key + "=" + sections[section][key] + "\n")
 
     @staticmethod
     def add_wgs84_projection_file(fname: Path):
@@ -122,8 +144,7 @@ class Utils():
         :param fname:
         :return:
         """
-        proj_str = \
-            'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
+        proj_str = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
         proj_file = open(fname, "w")
         proj_file.write(proj_str)
         proj_file.close()
@@ -136,10 +157,17 @@ class Utils():
         :param destination_path: path where final configuration is stored
         :return: copied base files
         """
-        assert source_dir.exists(), f"Cannot find source directory for base configuration: {source_dir}"
-        assert destination_dir.exists(), f"Cannot find destination directory for base configuration: {destination_dir}"
-        shutil.copytree(Path(source_dir).resolve(), Path(destination_dir).resolve(),
-                        dirs_exist_ok=dirs_exist_ok)
+        assert (
+            source_dir.exists()
+        ), f"Cannot find source directory for base configuration: {source_dir}"
+        assert (
+            destination_dir.exists()
+        ), f"Cannot find destination directory for base configuration: {destination_dir}"
+        shutil.copytree(
+            Path(source_dir).resolve(),
+            Path(destination_dir).resolve(),
+            dirs_exist_ok=dirs_exist_ok,
+        )
 
     @staticmethod
     def replace_tags_by_file(source_file, destination_file, tag_dict):
@@ -157,17 +185,19 @@ class Utils():
         if not Path(destination_file).exists():
             Path(destination_file).mkdir(parents=True)
 
-        destination_file_add = shutil.copy(source_file, destination_file, follow_symlinks=True)
+        destination_file_add = shutil.copy(
+            source_file, destination_file, follow_symlinks=True
+        )
         file_name = Path(destination_file_add).name
 
-        with open(destination_file_add, 'r') as f:
+        with open(destination_file_add, "r") as f:
             template = f.read()
             template_filled = template
             file_name_filled = file_name
             for key, value in tag_dict.items():
                 file_name_filled = re.sub("{" + key + "}", value, file_name_filled)
                 template_filled = re.sub("{" + key + "}", value, template_filled)
-                with open(destination_file_add, 'w') as ff:
+                with open(destination_file_add, "w") as ff:
                     ff.write(template_filled)
 
         old_file_name = Path(destination_file / file_name)
@@ -184,17 +214,27 @@ class Utils():
         :param destination_file: file with tags from source file replaced by values
         :return: all filled templates
         """
-        assert source_dir_templ.exists(), f"Cannot find source directory for templates: {source_dir_templ}"
-        assert destination_dir.exists(), f"Cannot find destination directory: {destination_dir}"
+        assert (
+            source_dir_templ.exists()
+        ), f"Cannot find source directory for templates: {source_dir_templ}"
+        assert (
+            destination_dir.exists()
+        ), f"Cannot find destination directory: {destination_dir}"
 
         tag_files = list(source_dir_templ.glob("**/*{*}.xml"))
         for file_path in tag_files:
             tag_files_paths_filled = file_path
             for key, value in tag_dict.items():
                 if key in str(tag_files_paths_filled):
-                    tag_files_paths_filled = re.sub("{" + key + "}", value, str(tag_files_paths_filled))
+                    tag_files_paths_filled = re.sub(
+                        "{" + key + "}", value, str(tag_files_paths_filled)
+                    )
                     if not "{" in tag_files_paths_filled:
-                        config_dir = re.sub(f"^.*?\\\\Config", "Config", tag_files_paths_filled)
+                        config_dir = re.sub(
+                            f"^.*?\\\\Config", "Config", tag_files_paths_filled
+                        )
                         destination_dir_add = Path(destination_dir / config_dir)
 
-                        Utils.replace_tags_by_file(file_path, destination_dir_add, tag_dict)
+                        Utils.replace_tags_by_file(
+                            file_path, destination_dir_add, tag_dict
+                        )
