062480416a80bf6d12313db77b34ae426cc7f877
DirkEilander <dirk.eilander@gmail.com>
2022-05-17 17:45:06 +0200

    bugfix resolve paths

--- hydromt/data_adapter.py
+++ hydromt/data_adapter.py
@@ -835,9 +835,9 @@ class DataAdapter(object, metaclass=ABCMeta):
         for date, var in product(dates, vrs):
             if hasattr(date, "month"):
                 yr, mth = date.year, date.month
-            path = path.format(year=yr, month=mth, variable=var)
+            path1 = path.format(year=yr, month=mth, variable=var)
             # FIXME: glob won't work with other than local file systems; use fsspec instead
-            fns.extend(glob.glob(path))
+            fns.extend(glob.glob(path1))
         if len(fns) == 0:
             raise FileNotFoundError(f"No such file found: {self.path}")
         return list(set(fns))  # return unique paths
