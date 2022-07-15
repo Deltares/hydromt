52a842c78b2dd6225e8c3e514fd43d83bb1dfae3
Anaïs Couasnon <couasnonanais@gmail.com>
2022-07-15 12:09:39 +0200

    Start of functions

--- hydromt/models/model_grid.py
+++ hydromt/models/model_grid.py
@@ -42,4 +42,72 @@ class GridModel(Model):
         """Method to write the complete model schematization and configuration to file."""
         super().write()
     
-    def setup_
+
+
+#%% original fucntions
+    def setup_lulcmaps(
+        self,
+        lulc_fn="globcover",
+        lulc_mapping_fn=None,
+        lulc_vars=[
+            "landuse",
+            "Kext",
+            "N",
+            "PathFrac",
+            "RootingDepth",
+            "Sl",
+            "Swood",
+            "WaterFrac",
+        ],
+    ):
+        """
+        This component derives several wflow maps are derived based on landuse-
+        landcover (LULC) data. 
+        
+        Currently, ``lulc_fn`` can be set to the "vito", "globcover"
+        or "corine", fo which lookup tables are constructed to convert lulc classses to
+        model parameters based on literature. The data is remapped at its original
+        resolution and then resampled to the model resolution using the average
+        value, unless noted differently.
+        
+        Adds model layers:
+        * **landuse** map: Landuse class [-]     
+        * **Kext** map: Extinction coefficient in the canopy gap fraction equation [-]
+        * **Sl** map: Specific leaf storage [mm]
+        * **Swood** map: Fraction of wood in the vegetation/plant [-]
+        * **RootingDepth** map: Length of vegetation roots [mm]
+        * **PathFrac** map: The fraction of compacted or urban area per grid cell [-]
+        * **WaterFrac** map: The fraction of open water per grid cell [-]
+        * **N** map: Manning Roughness [-]
+        Parameters
+        ----------
+        lulc_fn : {"globcover", "vito", "corine"}
+            Name of data source in data_sources.yml file.
+        lulc_mapping_fn : str
+            Path to a mapping csv file from landuse in source name to parameter values in lulc_vars.
+        lulc_vars : list
+            List of landuse parameters to keep.\
+            By default ["landuse","Kext","N","PathFrac","RootingDepth","Sl","Swood","WaterFrac"]
+        """
+        self.logger.info(f"Preparing LULC parameter maps.")
+        if lulc_mapping_fn is None:
+            fn_map = join(DATADIR, "lulc", f"{lulc_fn}_mapping.csv")
+        else:
+            fn_map = lulc_mapping_fn
+        if not isfile(fn_map):
+            self.logger.error(f"LULC mapping file not found: {fn_map}")
+            return
+        # read landuse map to DataArray
+        da = self.data_catalog.get_rasterdataset(
+            lulc_fn, geom=self.region, buffer=2, variables=["landuse"]
+        )
+        # process landuse
+        ds_lulc_maps = workflows.landuse(
+            da=da,
+            ds_like=self.staticmaps,
+            fn_map=fn_map,
+            params=lulc_vars,
+            logger=self.logger,
+        )
+        rmdict = {k: v for k, v in self._MAPS.items() if k in ds_lulc_maps.data_vars}
+        self.set_staticmaps(ds_lulc_maps.rename(rmdict))
--- hydromt/models/model_lumped.py
+++ hydromt/models/model_lumped.py
@@ -36,10 +36,45 @@ class LumpedModel(Model):
     def read(self):
         """Method to read the complete model schematization and configuration from file."""
         super().read()
+        # Other specifics to LumpedModel...
 
     def write(self):
         """Method to write the complete model schematization and configuration to file."""
         super().write()
+    
+    def set_forcing_name():
+
+    def space_interpolation():
+        #Check if more than one points are provided
+        #From 1D points to 2D -> using Thiessen polygons, others > check gdal
+        #Options: area average over selected region
+        #OPtions: picking new locations points (from list of points but also centroid, )
+        #Options: leave it as 2D?
+
+
+    def time_interpolation(): #For 1D value #multiple methods 
+        #--> set fillna as option
+        #Linear interpolation
+        #bfill, ffill
+        
+    def resample_freq(): # based on time steps of forcing, resample 1D time series
+    
+    def zonal_stats(): #2D from a scalar, we should also define variable and store this in a DataArray
+        #As arguments: 2D data, polygon shape, operation (mean, mode, max, min) --> return this
+        #return mode value of polygon within a raster --> the most frequent value
+        #retun mean 
+    
+    @property
+    def area(self):
+        #Check crs is in unit m
+    
+    def set_areas():
+        #Store the other areas as staticgeoms with keyname
+
+
+
+
+
 
 # need for zonalstatistics. Name of function = AreaAverage
 #test
