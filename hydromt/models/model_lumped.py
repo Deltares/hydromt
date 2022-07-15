3a339ea2e8d97e0f8c6a7d92d49b8fd7af7c4343
Daan Rooze <Daan.Rooze@deltares.nl>
2022-07-15 09:16:27 +0200

    added comment for test

--- hydromt/models/model_lumped.py
+++ hydromt/models/model_lumped.py
@@ -40,3 +40,5 @@ class LumpedModel(Model):
     def write(self):
         """Method to write the complete model schematization and configuration to file."""
         super().write()
+
+# need for zonalstatistics
