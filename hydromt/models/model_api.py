e18a9e1699f8759e7d94679ed0269cd62ac43e13
Dirk Eilander <dirk.eilander@gmail.com>
2022-10-06 15:19:36 +0200

    fix logger.warn bugs; remove unused import

--- hydromt/models/model_api.py
+++ hydromt/models/model_api.py
@@ -56,7 +56,7 @@ class Model(object, metaclass=ABCMeta):
         root: Optional[str] = None,
         mode: Optional[str] = "w",
         config_fn: Optional[str] = None,
-        data_libs: Optional[List[str]] = None,
+        data_libs: Union[List, str] = [],
         logger=logger,
         **artifact_keys,
     ):
