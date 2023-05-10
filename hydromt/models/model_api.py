d98769131de93b9f9e17bbc9f569be640014b238
Sam Vente <sam.vente@deltares.nl>
2023-05-10 09:42:01 +0200

    fix styling issues

--- hydromt/models/model_api.py
+++ hydromt/models/model_api.py
@@ -675,9 +675,11 @@ class Model(object, metaclass=ABCMeta):
             elif isfile(abspath(config_fn)):
                 cfdict = self._configread(abspath(config_fn))
                 self.logger.debug(f"{prefix} config read from {abspath(config_fn)}")
-            elif self._root is not None and isfile(join(self._root,config_fn)):
-                cfdict = self._configread(join(self.root,config_fn))
-                self.logger.debug(f"{prefix} config read from {join(self.root,config_fn)}")
+            elif self._root is not None and isfile(join(self._root, config_fn)):
+                cfdict = self._configread(join(self.root, config_fn))
+                self.logger.debug(
+                    f"{prefix} config read from {join(self.root,config_fn)}"
+                )
             elif not self._read and prefix != "Default":  # skip for missing default
                 self.logger.error(f"{prefix} config file not found at {config_fn}")
         self._config = cfdict
