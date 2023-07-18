b499c05ab0ddf8615796b2580ace7acecbeeb743
Sam Vente <savente93@gmail.com>
2023-07-18 13:45:00 +0200

    bump version

--- hydromt/__init__.py
+++ hydromt/__init__.py
@@ -1,7 +1,7 @@
 """HydroMT: Automated and reproducible model building and analysis."""
 
 # version number without 'v' at start
-__version__ = "0.7.2.dev0"
+__version__ = "0.8.0"
 
 # Set environment variables (this will be temporary)
 # to use shapely 2.0 in favor of pygeos (if installed)
