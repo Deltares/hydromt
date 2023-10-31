c6a79f83231cf99ba321bfab0859a991f3541595
Sam Vente <savente93@gmail.com>
2023-10-31 11:36:25 +0100

    split cli tests into multiple unit tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,34 +10,46 @@ from hydromt.cli import api as hydromt_api
 from hydromt.cli.main import main as hydromt_cli
 
 
-def test_cli(tmpdir):
+def test_cli_verison(tmpdir):
     r = CliRunner().invoke(hydromt_cli, "--version")
     assert r.exit_code == 0
     assert r.output.split()[-1] == __version__
 
+
+def test_cli_models(tmpdir):
     r = CliRunner().invoke(hydromt_cli, "--models")
     assert r.exit_code == 0
     assert r.output.startswith("model plugins")
 
+
+def test_cli_help(tmpdir):
     r = CliRunner().invoke(hydromt_cli, "--help")
     assert r.exit_code == 0
     # NOTE: when called from CliRunner we get "Usage: main" instead of "Usage: hydromt"
     assert r.output.startswith("Usage: main [OPTIONS] COMMAND [ARGS]...")
 
+
+def test_cli_build_help(tmpdir):
     r = CliRunner().invoke(hydromt_cli, ["build", "--help"])
     assert r.exit_code == 0
     assert r.output.startswith("Usage: main build [OPTIONS] MODEL MODEL_ROOT")
 
+
+def test_cli_update_help(tmpdir):
     r = CliRunner().invoke(hydromt_cli, ["update", "--help"])
     assert r.exit_code == 0
     assert r.output.startswith("Usage: main update [OPTIONS] MODEL MODEL_ROOT")
 
+
+def test_cli_clip_help(tmpdir):
     r = CliRunner().invoke(hydromt_cli, ["clip", "--help"])
     assert r.exit_code == 0
     assert r.output.startswith(
         "Usage: main clip [OPTIONS] MODEL MODEL_ROOT MODEL_DESTINATION REGION"
     )
 
+
+def test_cli_build_grid_model(tmpdir):
     root = str(tmpdir.join("grid_model_region"))
     cmd = [
         "build",
@@ -49,52 +61,58 @@ def test_cli(tmpdir):
         "setup_grid.res=0.05",
         "-vv",
     ]
-    r = CliRunner().invoke(hydromt_cli, cmd)
+    _ = CliRunner().invoke(hydromt_cli, cmd)
 
     # test force overwrite
-    r = CliRunner().invoke(hydromt_cli, cmd)
     with pytest.raises(IOError, match="Model dir already exists"):
-        raise r.exception
+        _ = CliRunner().invoke(hydromt_cli, cmd, catch_exceptions=False)
 
     r = CliRunner().invoke(hydromt_cli, cmd + ["--fo"])
     assert r.exit_code == 0
 
-    root = str(tmpdir.join("empty_region"))
-    r = CliRunner().invoke(
-        hydromt_cli,
-        ["build", "grid_model", root, "-vv"],
-    )
-    assert r.exit_code == 0
 
-    r = CliRunner().invoke(
-        hydromt_cli,
-        [
-            "build",
-            "test_model",
-            str(tmpdir),
-            "-r",
-            "{'subbasin': [-7.24, 62.09], 'strord': 4}",
-        ],
-    )
+def test_cli_build_unknown_model(tmpdir):
     with pytest.raises(ValueError, match="Unknown model"):
-        raise r.exception
+        _ = CliRunner().invoke(
+            hydromt_cli,
+            [
+                "build",
+                "test_model",
+                str(tmpdir),
+                "-r",
+                "{'subbasin': [-7.24, 62.09], 'strord': 4}",
+            ],
+            catch_exceptions=False,
+        )
 
-    r = CliRunner().invoke(
-        hydromt_cli,
-        ["update", "test_model", str(tmpdir), "-c", "component", "--opt", "key=value"],
-    )
+
+def test_cli_update_unknown_model(tmpdir):
     with pytest.raises(ValueError, match="Unknown model"):
-        raise r.exception
+        _ = CliRunner().invoke(
+            hydromt_cli,
+            [
+                "update",
+                "test_model",
+                str(tmpdir),
+                "-c",
+                "component",
+                "--opt",
+                "key=value",
+            ],
+            catch_exceptions=False,
+        )
 
-    r = CliRunner().invoke(
-        hydromt_cli,
-        ["clip", "test_model", str(tmpdir), str(tmpdir), "{'bbox': [1,2,3,4]}"],
-    )
+
+def test_cli_clip_unknown_model(tmpdir):
     with pytest.raises(NotImplementedError):
-        raise r.exception
+        _ = CliRunner().invoke(
+            hydromt_cli,
+            ["clip", "test_model", str(tmpdir), str(tmpdir), "{'bbox': [1,2,3,4]}"],
+            catch_exceptions=False,
+        )
 
 
-def test_export_cli(tmpdir):
+def test_export_cli_deltared_data(tmpdir):
     r = CliRunner().invoke(
         hydromt_cli,
         [
@@ -104,11 +122,14 @@ def test_export_cli(tmpdir):
             "river_atlas_v10",
             "-r",
             "{'subbasin': [-7.24, 62.09], 'uparea': 50}",
-            "-dd",
+            "--dd",
         ],
     )
 
     assert r.exit_code == 0, r.output
+
+
+def test_export_cli_catalog(tmpdir):
     r = CliRunner().invoke(
         hydromt_cli,
         [
@@ -117,11 +138,13 @@ def test_export_cli(tmpdir):
             "-t",
             "river_atlas_v10",
             "-d",
-            "path/to/catalog.yml",
+            "tests/data/test_sources.yml",
         ],
     )
     assert r.exit_code == 0, r.output
 
+
+def test_export_cli_config_file(tmpdir):
     r = CliRunner().invoke(
         hydromt_cli,
         ["export", ".", "-f", "tests/data/export_config.yml"],
