635b5b6845aaaaebdfd9c2c82dc9b2f95e5c3a34
Sam Vente <savente93@gmail.com>
2023-11-08 11:21:55 +0100

    Add validation methods for data catalog (#632)

---
+++ hydromt/validators/data_catalog.py
@@ -0,0 +1,164 @@
+"""Pydantic models for the validation of Data catalogs."""
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
+
+from pydantic import AnyUrl, BaseModel, ConfigDict
+from pydantic.fields import Field
+from pydantic_core import Url
+
+from hydromt.data_catalog import _yml_from_uri_or_path
+from hydromt.typing import Bbox, Number, TimeRange
+
+
+class SourceSpecDict(BaseModel):
+    """A complete source variant specification."""
+
+    source: str
+    provider: Optional[str] = None
+    version: Optional[Union[str, int]] = None
+
+    @staticmethod
+    def from_dict(input_dict):
+        """Create a source variant specification from a dictionary."""
+        return SourceSpecDict(**input_dict)
+
+
+class Extent(BaseModel):
+    """A validation model for describing the space and time a dataset covers."""
+
+    time_range: TimeRange
+    bbox: Bbox
+
+
+class DataCatalogMetaData(BaseModel):
+    """The metadata section of a Hydromt data catalog."""
+
+    root: Optional[Path] = None
+    version: Optional[Union[str, int]] = None
+    name: Optional[str] = None
+    model_config: ConfigDict = ConfigDict(
+        str_strip_whitespace=True,
+        extra="allow",
+    )
+
+    @staticmethod
+    def from_dict(input_dict: Optional[Dict]) -> "DataCatalogMetaData":
+        """Convert a dictionary into a validated data catalog metadata item."""
+        if input_dict is None:
+            return DataCatalogMetaData()
+        else:
+            return DataCatalogMetaData(**input_dict)
+
+
+class DataCatalogItemMetadata(BaseModel):
+    """The metadata for a data source."""
+
+    category: Optional[str] = None
+    paper_doi: Optional[str] = None
+    paper_ref: Optional[str] = None
+    source_license: Optional[str] = None
+    source_url: Optional[AnyUrl] = None
+    source_version: Optional[str] = None
+    notes: Optional[str] = None
+
+    model_config: ConfigDict = ConfigDict(
+        str_strip_whitespace=True, coerce_numbers_to_str=True
+    )
+
+    @staticmethod
+    def from_dict(input_dict):
+        """Convert a dictionary into a validated source metadata item."""
+        if input_dict is None:
+            return DataCatalogItemMetadata()
+        else:
+            item_source_url = input_dict.pop("source_url", None)
+            if item_source_url:
+                Url(item_source_url)
+            return DataCatalogItemMetadata(**input_dict, source_url=item_source_url)
+
+
+class DataCatalogItem(BaseModel):
+    """A validated data source."""
+
+    name: str
+    data_type: str
+    driver: str
+    path: Path
+    crs: Optional[int] = None
+    filesystem: Optional[str] = None
+    kwargs: Dict[str, Any] = Field(default_factory=dict)
+    storage_options: Dict[str, Any] = Field(default_factory=dict)
+    rename: Dict[str, str] = Field(default_factory=dict)
+    nodata: Optional[Number] = None
+    meta: Optional[DataCatalogItemMetadata] = None
+    unit_add: Optional[Dict[str, Number]] = None
+    unit_mult: Optional[Dict[str, Number]] = None
+
+    model_config: ConfigDict = ConfigDict(
+        str_strip_whitespace=True,
+        extra="forbid",
+    )
+
+    @staticmethod
+    def from_dict(input_dict, name=None):
+        """Convert a dictionary into a validated source item."""
+        dict_name = input_dict.pop("name", None)
+        if name is None:
+            entry_name = dict_name
+        else:
+            entry_name = name
+        item_metadata = DataCatalogItemMetadata.from_dict(input_dict.pop("meta", {}))
+        item_kwargs = input_dict.pop("kwargs", {})
+        item_storage_options = input_dict.pop("storage_options", {})
+        return DataCatalogItem(
+            **input_dict,
+            name=entry_name,
+            kwargs=item_kwargs,
+            storage_options=item_storage_options,
+            meta=item_metadata,
+        )
+
+
+class DataCatalogValidator(BaseModel):
+    """A validated complete data catalog."""
+
+    meta: Optional[DataCatalogMetaData] = None
+    sources: Dict[str, DataCatalogItem] = Field(default_factory=dict)
+    aliases: Dict[str, str] = Field(default_factory=dict)
+
+    model_config: ConfigDict = ConfigDict(
+        str_strip_whitespace=True,
+        extra="forbid",
+    )
+
+    @staticmethod
+    def from_dict(input_dict):
+        """Create a validated datacatalog from a dictionary."""
+        if input_dict is None:
+            return DataCatalogValidator()
+        else:
+            meta = input_dict.pop("meta", None)
+            catalog_meta = DataCatalogMetaData.from_dict(meta)
+            catalog_entries = {}
+            catalog_aliases = {}
+            for entry_name, entry_dict in input_dict.items():
+                if "alias" in entry_dict.keys():
+                    catalog_aliases[entry_name] = entry_dict["alias"]
+                else:
+                    catalog_entries[entry_name] = DataCatalogItem.from_dict(
+                        entry_dict, name=entry_name
+                    )
+
+            for src, dst in catalog_aliases.items():
+                assert (
+                    dst in catalog_entries.keys()
+                ), f"{src} references unfound entry {dst}"
+            return DataCatalogValidator(
+                meta=catalog_meta, sources=catalog_entries, aliases=catalog_aliases
+            )
+
+    @staticmethod
+    def from_yml(path: str):
+        """Create a validated datacatalog loaded from the provided path."""
+        yml_dict = _yml_from_uri_or_path(path)
+        return DataCatalogValidator.from_dict(yml_dict)
---
+++ tests/validators/test_data_catalog_validation.py
@@ -0,0 +1,168 @@
+"""Testing of the Pydantic models for validation of Data catalogs."""
+
+
+from pathlib import Path
+
+import pytest
+from pydantic import ValidationError
+from pydantic_core import Url
+
+from hydromt.validators.data_catalog import (
+    DataCatalogItem,
+    DataCatalogMetaData,
+    DataCatalogValidator,
+)
+
+
+def test_geodataframe_entry_validation():
+    d = {
+        "crs": 4326,
+        "data_type": "GeoDataFrame",
+        "driver": "vector",
+        "kwargs": {"layer": "BasinATLAS_v10_lev12"},
+        "meta": {
+            "category": "hydrography",
+            "notes": "renaming and units might require some revision",
+            "paper_doi": "10.1038/s41597-019-0300-6",
+            "paper_ref": "Linke et al. (2019)",
+            "source_license": "CC BY 4.0",
+            "source_url": "https://www.hydrosheds.org/hydroatlas",
+            "source_version": "10",
+        },
+        "path": "hydrography/hydro_atlas/basin_atlas_v10.gpkg",
+    }
+    entry = DataCatalogItem.from_dict(d, name="basin_atlas_level12_v10")
+
+    assert entry.crs == 4326
+    assert entry.data_type == "GeoDataFrame"
+    assert entry.driver == "vector"
+    assert entry.kwargs == {"layer": "BasinATLAS_v10_lev12"}
+    assert entry.meta is not None
+    assert entry.meta.category == "hydrography"
+    assert entry.meta.notes == "renaming and units might require some revision"
+    assert entry.meta.paper_doi == "10.1038/s41597-019-0300-6"
+    assert entry.meta.paper_ref == "Linke et al. (2019)"
+    assert entry.meta.source_license == "CC BY 4.0"
+    assert entry.meta.source_url == Url("https://www.hydrosheds.org/hydroatlas")
+    assert entry.meta.source_version == "10"
+    assert entry.path == Path("hydrography/hydro_atlas/basin_atlas_v10.gpkg")
+
+
+def test_valid_catalog_with_alias():
+    d = {
+        "chelsa": {"alias": "chelsa_v1.2"},
+        "chelsa_v1.2": {
+            "crs": 4326,
+            "data_type": "RasterDataset",
+            "driver": "raster",
+            "kwargs": {
+                "chunks": {
+                    "x": 3600,
+                    "y": 3600,
+                }
+            },
+            "meta": {
+                "category": "meteo",
+                "paper_doi": "10.1038/sdata.2017.122",
+                "paper_ref": "Karger et al. (2017)",
+                "source_license": "CC BY 4.0",
+                "source_url": "http://chelsa-climate.org/downloads/",
+                "source_version": "1.2",
+            },
+            "path": "meteo/chelsa_clim_v1.2/CHELSA_bio10_12.tif",
+        },
+    }
+    _ = DataCatalogValidator.from_dict(d)
+
+
+def test_dangling_alias_catalog_entry():
+    d = {
+        "chelsa": {"alias": "chelsa_v1.2"},
+    }
+
+    with pytest.raises(AssertionError):
+        _ = DataCatalogValidator.from_dict(d)
+
+
+def test_valid_alias_catalog_entry():
+    d = {
+        "chelsa": {"alias": "chelsa_v1.2"},
+        "chelsa_v1.2": {
+            "crs": 4326,
+            "data_type": "RasterDataset",
+            "driver": "raster",
+            "path": ".",
+        },
+    }
+    entry = DataCatalogValidator.from_dict(d)
+
+    assert entry.aliases == {"chelsa": "chelsa_v1.2"}
+
+
+def test_catalog_metadata_validation():
+    d = {
+        "root": "p:/wflow_global/hydromt",
+        "version": "2023.3",
+    }
+    catalog_metadata = DataCatalogMetaData.from_dict(d)
+    assert catalog_metadata.root == Path("p:/wflow_global/hydromt")
+    assert catalog_metadata.version == "2023.3"
+
+
+def test_raster_dataset_entry_validation():
+    d = {
+        "crs": 4326,
+        "data_type": "RasterDataset",
+        "driver": "raster",
+        "kwargs": {
+            "chunks": {
+                "x": 3600,
+                "y": 3600,
+            }
+        },
+        "meta": {
+            "category": "meteo",
+            "paper_doi": "10.1038/sdata.2017.122",
+            "paper_ref": "Karger et al. (2017)",
+            "source_license": "CC BY 4.0",
+            "source_url": "http://chelsa-climate.org/downloads/",
+            "source_version": "1.2",
+        },
+        "path": "meteo/chelsa_clim_v1.2/CHELSA_bio10_12.tif",
+    }
+
+    entry = DataCatalogItem.from_dict(d, name="chelsa_v1.2")
+    assert entry.name == "chelsa_v1.2"
+    assert entry.crs == 4326
+    assert entry.data_type == "RasterDataset"
+    assert entry.driver == "raster"
+    assert entry.path == Path("meteo/chelsa_clim_v1.2/CHELSA_bio10_12.tif")
+    assert entry.kwargs == {"chunks": {"x": 3600, "y": 3600}}
+    assert entry.meta is not None
+    assert entry.meta.category == "meteo"
+    assert entry.meta.paper_doi == "10.1038/sdata.2017.122"
+    assert entry.meta.paper_ref == "Karger et al. (2017)"
+    assert entry.meta.source_license == "CC BY 4.0"
+    assert entry.meta.source_url == Url("http://chelsa-climate.org/downloads/")
+    assert entry.meta.source_version == "1.2"
+
+
+def test_dataset_entry_with_typo_validation():
+    d = {
+        "crs_num": 4326,
+        "datatype": "RasterDataset",
+        "diver": "raster",
+        "kw_args": {
+            "chunks": {
+                "x": 3600,
+                "y": 3600,
+            }
+        },
+        "filepath": "meteo/chelsa_clim_v1.2/CHELSA_bio10_12.tif",
+    }
+
+    # 8 errors are:
+    #  - missing crs, data_type, driver and path (4)
+    #  - extra crs_num, datatype, diver, and filepath (5)
+    with pytest.raises(ValidationError, match="8 validation errors"):
+        _ = DataCatalogItem.from_dict(d, name="chelsa_v1.2")
