aa308d8ce5d2bcb685dde028abcb023933146f54
Jaap <33715902+Jaapel@users.noreply.github.com>
2024-01-12 12:43:52 +0100

    add gpkg test

--- tests/test_io.py
+++ tests/test_io.py
@@ -26,6 +26,7 @@ def test_open_vector(engine, tmpdir, df, geodf, world):
     fn_xls = str(tmpdir.join("test.xlsx"))
     fn_geojson = str(tmpdir.join("test.geojson"))
     fn_shp = str(tmpdir.join("test.shp"))
+    fn_gpkg = str(tmpdir.join("test.gpkg"))
     df.to_csv(fn_csv)
     df.to_parquet(fn_parquet)
     if _compat.HAS_OPENPYXL:
@@ -33,6 +34,7 @@ def test_open_vector(engine, tmpdir, df, geodf, world):
     geodf.to_file(fn_geojson, driver="GeoJSON")
     hydromt.write_xy(fn_xy, geodf)
     geodf.to_file(fn_shp)
+    geodf.to_file(fn_gpkg, driver="GPKG")
     # read csv
     gdf1 = hydromt.open_vector(fn_csv, assert_gtype="Point", crs=4326)
     assert gdf1.crs == geodf.crs
@@ -48,7 +50,11 @@ def test_open_vector(engine, tmpdir, df, geodf, world):
     # read xy
     gdf1 = hydromt.open_vector(fn_xy, crs=4326)
     assert np.all(gdf1 == geodf[["geometry"]])
+    # read shapefile
     gdf1 = hydromt.open_vector(fn_shp)
+    assert np.all(gdf1 == geodf)
+    # read geopackage
+    gdf1 = hydromt.open_vector(fn_gpkg)
     assert np.all(gdf1 == geodf)
     # read parquet
     gdf1 = hydromt.open_vector(fn_parquet, crs=4326)
