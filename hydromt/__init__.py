39f32b592cc6482b10967f6c81715556aa1f330c
Sam Vente <savente93@gmail.com>
2024-02-08 16:44:16 +0100

    update version number

--- hydromt/__init__.py
+++ hydromt/__init__.py
@@ -1,37 +1,13 @@
 """HydroMT: Automated and reproducible model building and analysis."""
 
 # version number without 'v' at start
-__version__ = "0.9.2"
+__version__ = "1.0.0-alpha"
 
 # pkg_resource deprication warnings originate from dependencies
 # so silence them for now
 import warnings
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-
-# # required for accessor style documentation
-# from xarray import DataArray, Dataset  # noqa: F401
-
-# # submodules
-# from hydromt import cli, gis, io, models, stats, workflows
-
-# # high-level methods
-# from hydromt.models import GridModel, Model, ModelCatalog, NetworkModel, VectorModel
-
-# __all__ = [
-#     "cli",
-#     "gis",
-#     "io",
-#     "stats",
-#     "workflows",
-#     "models",
-#     "GridModel",
-#     "Model",
-#     "ModelCatalog",
-#     "NetworkModel",
-#     "VectorModel",
-# ]
 
 
 # required for accessor style documentation
