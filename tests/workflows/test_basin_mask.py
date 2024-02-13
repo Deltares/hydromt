466fe15d7cbee338caaba06986bf6c339a773514
Sam Vente <savente93@gmail.com>
2024-02-13 13:04:32 +0100

    move region tests to model region

--- tests/workflows/test_basin_mask.py
+++ tests/workflows/test_basin_mask.py
@@ -5,184 +5,14 @@ import logging
 import numpy as np
 import pytest
 
-from hydromt import DataCatalog, raster
+import hydromt
+from hydromt import raster
 from hydromt.workflows.basin_mask import (
     _check_size,
     get_basin_geometry,
 )
 
-logger = logging.getLogger("tets_basin")
-
-
-# def test_region(tmpdir, world, geodf, rioda):
-#     # prepare test data
-#     fn_gdf = str(tmpdir.join("world.geojson"))
-#     world.to_file(fn_gdf, driver="GeoJSON")
-#     fn_grid = str(tmpdir.join("grid.tif"))
-#     rioda.raster.to_raster(fn_grid)
-#     cat = DataCatalog()
-#     cat.from_dict(
-#         {
-#             "world": {
-#                 "path": fn_gdf,
-#                 "data_type": "GeoDataFrame",
-#                 "driver": "vector",
-#             },
-#             "grid": {
-#                 "path": fn_grid,
-#                 "data_type": "RasterDataset",
-#                 "driver": "raster",
-#             },
-#         }
-#     )
-
-#     # model
-#     region = {"region": [0.0, -1.0]}
-#     with pytest.raises(ValueError, match=r"Region key .* not understood.*"):
-#         parse_region(region)
-
-#     model = MODELS.generic[0]
-#     root = str(tmpdir.join(model)) + "_test_region"
-#     if not os.path.isdir(root):
-#         os.mkdir(root)
-#     region = {model: root}
-#     kind, region = parse_region(region)
-#     assert kind == "model"
-
-#     # geom
-#     kind, region = parse_region({"geom": world})
-#     assert isinstance(region["geom"], gpd.GeoDataFrame)
-#     kind, region = parse_region({"geom": fn_gdf})
-#     assert isinstance(region["geom"], gpd.GeoDataFrame)
-#     kind, region = parse_region({"geom": "world"}, data_catalog=cat)
-#     assert isinstance(region["geom"], gpd.GeoDataFrame)
-#     # geom:  points should fail
-#     region = {"geom": geodf}
-#     with pytest.raises(ValueError, match=r"Region value.*"):
-#         kind, region = parse_region(region)
-
-#     # grid
-#     kind, region = parse_region({"grid": rioda})
-#     assert isinstance(region["grid"], xr.DataArray)
-#     kind, region = parse_region({"grid": fn_grid})
-#     assert isinstance(region["grid"], xr.DataArray)
-#     kind, region = parse_region({"grid": "grid"}, data_catalog=cat)
-#     assert isinstance(region["grid"], xr.DataArray)
-
-#     # basid
-#     region = {"basin": [1001, 1002, 1003, 1004, 1005]}
-#     kind, region = parse_region(region)
-#     assert kind == "basin"
-#     assert region.get("basid") == [1001, 1002, 1003, 1004, 1005]
-#     region = {"basin": 101}
-#     kind, region = parse_region(region)
-#     assert kind == "basin"
-#     assert region.get("basid") == 101
-
-#     # bbox
-#     region = {"outlet": [0.0, -5.0, 3.0, 0.0]}
-#     kind, region = parse_region(region)
-#     assert kind == "outlet"
-#     assert "bbox" in region
-
-#     # xy
-#     region = {"subbasin": [1.0, -1.0], "uparea": 5.0, "bounds": [0.0, -5.0, 3.0, 0.0]}
-#     kind, region = parse_region(region)
-#     assert kind == "subbasin"
-#     assert "xy" in region
-#     assert "bounds" in region
-#     region = {"basin": [[1.0, 1.5], [0.0, -1.0]]}
-#     kind, region = parse_region(region)
-#     assert "xy" in region
-#     region = {"interbasin": geodf}
-#     kind, region = parse_region(region)
-#     assert "xy" in region
-
-
-# @pytest.mark.skip(reason="Needs refactor to Pydantic BaseModel.")
-# def test_region(tmpdir, world, geodf, rioda):
-#     # prepare test data
-#     fn_gdf = str(tmpdir.join("world.geojson"))
-#     world.to_file(fn_gdf, driver="GeoJSON")
-#     fn_grid = str(tmpdir.join("grid.tif"))
-#     rioda.raster.to_raster(fn_grid)
-#     cat = DataCatalog()
-#     cat.from_dict(
-#         {
-#             "world": {
-#                 "path": fn_gdf,
-#                 "data_type": "GeoDataFrame",
-#                 "driver": "vector",
-#             },
-#             "grid": {
-#                 "path": fn_grid,
-#                 "data_type": "RasterDataset",
-#                 "driver": "raster",
-#             },
-#         }
-#     )
-
-#     # model
-#     region = {"region": [0.0, -1.0]}
-#     with pytest.raises(ValueError, match=r"Region key .* not understood.*"):
-#         parse_region(region)
-
-#     model = MODELS.generic[0]
-#     root = str(tmpdir.join(model)) + "_test_region"
-#     if not os.path.isdir(root):
-#         os.mkdir(root)
-#     region = {model: root}
-#     kind, region = parse_region(region)
-#     assert kind == "model"
-
-#     # geom
-#     kind, region = parse_region({"geom": world})
-#     assert isinstance(region["geom"], gpd.GeoDataFrame)
-#     kind, region = parse_region({"geom": fn_gdf})
-#     assert isinstance(region["geom"], gpd.GeoDataFrame)
-#     kind, region = parse_region({"geom": "world"}, data_catalog=cat)
-#     assert isinstance(region["geom"], gpd.GeoDataFrame)
-#     # geom:  points should fail
-#     region = {"geom": geodf}
-#     with pytest.raises(ValueError, match=r"Region value.*"):
-#         kind, region = parse_region(region)
-
-#     # grid
-#     kind, region = parse_region({"grid": rioda})
-#     assert isinstance(region["grid"], xr.DataArray)
-#     kind, region = parse_region({"grid": fn_grid})
-#     assert isinstance(region["grid"], xr.DataArray)
-#     kind, region = parse_region({"grid": "grid"}, data_catalog=cat)
-#     assert isinstance(region["grid"], xr.DataArray)
-
-#     # basid
-#     region = {"basin": [1001, 1002, 1003, 1004, 1005]}
-#     kind, region = parse_region(region)
-#     assert kind == "basin"
-#     assert region.get("basid") == [1001, 1002, 1003, 1004, 1005]
-#     region = {"basin": 101}
-#     kind, region = parse_region(region)
-#     assert kind == "basin"
-#     assert region.get("basid") == 101
-
-#     # bbox
-#     region = {"outlet": [0.0, -5.0, 3.0, 0.0]}
-#     kind, region = parse_region(region)
-#     assert kind == "outlet"
-#     assert "bbox" in region
-
-#     # xy
-#     region = {"subbasin": [1.0, -1.0], "uparea": 5.0, "bounds": [0.0, -5.0, 3.0, 0.0]}
-#     kind, region = parse_region(region)
-#     assert kind == "subbasin"
-#     assert "xy" in region
-#     assert "bounds" in region
-#     region = {"basin": [[1.0, 1.5], [0.0, -1.0]]}
-#     kind, region = parse_region(region)
-#     assert "xy" in region
-#     region = {"interbasin": geodf}
-#     kind, region = parse_region(region)
-#     assert "xy" in region
+logger = logging.getLogger(__name__)
 
 
 def test_check_size(caplog):
@@ -204,7 +34,7 @@ def test_check_size(caplog):
 @pytest.mark.skip(reason="Needs implementation of RasterDataSet.")
 @pytest.mark.filterwarnings("ignore::UserWarning")
 def test_basin(caplog):
-    data_catalog = DataCatalog("artifact_data", logger=logger)
+    data_catalog = hydromt.DataCatalog("artifact_data", logger=logger)
     ds = data_catalog.get_rasterdataset("merit_hydro_1k")
     gdf_bas_index = data_catalog.get_geodataframe("merit_hydro_index")
     bas_index = data_catalog.get_source("merit_hydro_index")
