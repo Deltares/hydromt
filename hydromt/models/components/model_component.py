01f26cf847b9498d3b0f102b91ffad4a518d87e8
Tjalling-dejong <tjalling.dejong@deltares.nl>
2024-02-21 16:44:02 +0100

    updated abstract ModelComponent class

--- hydromt/models/components/model_component.py
+++ hydromt/models/components/model_component.py
@@ -1,10 +1,26 @@
 """Abstract base class for ModelComponent."""
-
 from abc import ABC, abstractmethod
+from logging import Logger
+
+from hydromt.data_catalog import DataCatalog
+from hydromt.models.root import ModelRoot
 
 
 class ModelComponent(ABC):
     """Abstract base class for ModelComponent."""
+
+    @abstractmethod
+    def __init__(
+        self,
+        root: ModelRoot,
+        data_catalog: DataCatalog,
+        model_region,  # TODO: add ModelRegion object when finished
+        logger: Logger,
+    ):
+        self.logger = logger
+        self.root = root
+        self.data_catalog = data_catalog
+        self.model_region = model_region
 
     @abstractmethod
     def set(self):
