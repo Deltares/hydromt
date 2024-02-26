59eac3a3dc7d1e41a76e592ebf8e9a9e6032e746
Jaap Langemeijer <33715902+Jaapel@users.noreply.github.com>
2024-02-26 13:39:01 +0100

    add check for warnings (#794)

--- tests/drivers/test_pyogrio_driver.py
+++ tests/drivers/test_pyogrio_driver.py
@@ -1,3 +1,4 @@
+import warnings
 from pathlib import Path
 
 import geopandas as gpd
@@ -12,22 +13,62 @@ from hydromt.drivers.pyogrio_driver import PyogrioDriver
 
 class TestPyogrioDriver:
     @pytest.fixture(scope="class")
-    def uri(self, geodf: gpd.GeoDataFrame, tmp_dir: Path) -> str:
+    def uri_gjson(self, geodf: gpd.GeoDataFrame, tmp_dir: Path) -> str:
         uri = str(tmp_dir / "test.geojson")
         geodf.to_file(uri, driver="GeoJSON")
         return uri
 
-    def test_read(self, uri: str, geodf: gpd.GeoDataFrame):
-        driver = PyogrioDriver()
-        gdf: gpd.GeoDataFrame = driver.read(uri)
-        assert np.all(gdf == geodf)
+    @pytest.fixture(scope="class")
+    def uri_shp(self, geodf: gpd.GeoDataFrame, tmp_dir: Path) -> str:
+        uri = str(tmp_dir / "test.shp")
+        geodf.to_file(uri, "ESRI Shapefile")
+        return uri
 
+    @pytest.fixture(scope="class")
+    def uri_gpkg(self, geodf: gpd.GeoDataFrame, tmp_dir: Path) -> str:
+        uri = str(tmp_dir / "test.gpkg")
+        geodf.to_file(uri, driver="GPKG")
+        return uri
+
+    @pytest.fixture(scope="class")
+    def uri_fgb(self, geodf: gpd.GeoDataFrame, tmp_dir: Path) -> str:
+        uri = str(tmp_dir / "test.fgb")
+        geodf.to_file(uri, driver="FlatGeobuf")
+        return uri
+
+    # lazy-fixtures not maintained:
+    # https://github.com/TvoroG/pytest-lazy-fixture/issues/65#issuecomment-1914527162
+    fixture_uris = pytest.mark.parametrize(
+        "uri",
+        ["uri_gjson", "uri_shp", "uri_fgb", "uri_gpkg"],
+    )
+
+    @pytest.fixture(scope="class")
+    def _raise_gdal_warnings(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+            yield
+
+    @pytest.mark.usefixtures("_raise_gdal_warnings")
+    @fixture_uris
+    def test_read(
+        self, uri: str, request: pytest.FixtureRequest, geodf: gpd.GeoDataFrame
+    ):
+        uri = request.getfixturevalue(uri)
+        driver = PyogrioDriver()
+        gdf: gpd.GeoDataFrame = driver.read(uri).sort_values("id")
+        assert np.all(gdf.reset_index(drop=True) == geodf)  # fgb scrambles order
+
+    @pytest.mark.usefixtures("_raise_gdal_warnings")
     def test_read_nodata(self):
         driver = PyogrioDriver()
         with pytest.raises(DataSourceError):
             driver.read("no_data.geojson")
 
-    def test_read_with_filters(self, uri: str):
+    @pytest.mark.usefixtures("_raise_gdal_warnings")
+    @fixture_uris
+    def test_read_with_filters(self, uri: str, request: pytest.FixtureRequest):
+        uri = request.getfixturevalue(uri)
         driver = PyogrioDriver()
         bbox: Bbox = (-60, -34.5600, -55, -30)
         gdf: gpd.GeoDataFrame = driver.read(
