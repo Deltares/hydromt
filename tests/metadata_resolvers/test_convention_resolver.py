b6c923fcf79676599b60442239b0b0065d793787
Jaap <33715902+Jaapel@users.noreply.github.com>
2024-03-08 15:37:26 +0100

    convention resolver test works

--- tests/metadata_resolvers/test_convention_resolver.py
+++ tests/metadata_resolvers/test_convention_resolver.py
@@ -1,34 +1,21 @@
-import pytest
-
-from hydromt.data_sources.rasterdataset import RasterDataSource
-from hydromt.drivers.zarr_driver import ZarrDriver
 from hydromt.metadata_resolvers.convention_resolver import ConventionResolver
 
 
-@pytest.fixture()
-def mock_source() -> RasterDataSource:
-    return RasterDataSource(
-        name="test",
-        uri="/{unknown_key}_{zoom_level}_{variable}_{year}_{month:02d}.nc",
-        metadata_resolver=ConventionResolver(),
-        driver=ZarrDriver(),
-    )
-
-
 class TestConventionResolver:
-    def test_resolve(self, mock_source: RasterDataSource):
+    def test_resolve(self):
+        uri = "/{unknown_key}_{zoom_level}_{variable}_{year}_{month:02d}.nc"
         resolver = ConventionResolver()
         # test
-        uris = resolver.resolve(mock_source)
+        uris = resolver.resolve(uri)
         assert len(uris) == 1
         assert uris[0] == "/{unknown_key}_0_*_*_*.nc"
 
-        uris = resolver.resolve(mock_source, variables=["precip"])
+        uris = resolver.resolve(uri, variables=["precip"])
         assert len(uris) == 1
         assert uris[0] == "/{unknown_key}_0_precip_*_*.nc"
 
         uris = resolver.resolve(
-            mock_source, timerange=("2021-03-01", "2021-05-01"), variables=["precip"]
+            uri, timerange=("2021-03-01", "2021-05-01"), variables=["precip"]
         )
         assert len(uris) == 3
         assert uris[0] == "/{unknown_key}_0_precip_2021_03.nc"
