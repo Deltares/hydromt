55fe985340d20d7b61c8cd60c6b49e3d8214f1d6
Sam Vente <savente93@proton.me>
2024-03-13 14:56:20 +0100

    move plugins.py

---
+++ hydromt/plugins.py
@@ -0,0 +1,68 @@
+"""Implementation of the mechanism to access the plugin entrypoints."""
+
+import logging
+from typing import TYPE_CHECKING, Dict, Type
+
+from importlib_metadata import entry_points
+
+if TYPE_CHECKING:
+    from hydromt.models.components import ModelComponent  # noqa
+    from hydromt.models import Model  # noqa
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["PLUGINS"]
+
+
+def _discover_plugins(group: str) -> Dict[str, Type]:
+    plugins = {}
+    eps = entry_points(group=group)
+    for ep in eps:
+        module = ep.load()
+        for attr_name in module.__all__:
+            attr = getattr(module, attr_name)
+            if attr_name not in plugins:
+                plugins[attr_name] = attr
+            else:
+                raise ValueError(f"Conflicting definitions for component {attr_name}")
+
+    return plugins
+
+
+class Plugins:
+    """The model catalogue provides access to plugins."""
+
+    def __init__(self):
+        """Initiate the catalog object."""
+        self._component_plugins = None
+        self._model_plugins = None
+
+    @property
+    def component_plugins(self):
+        """Load and provide access to all known component plugins."""
+        if self._component_plugins is None:
+            self._component_plugins = _discover_plugins(
+                group="hydromt.models.components"
+            )
+
+        return self._component_plugins
+
+    @property
+    def model_plugins(self):
+        """Load and provide access to all known model plugins."""
+        if self._model_plugins is None:
+            self._model_plugins = _discover_plugins(group="hydromt.models.components")
+
+        return self._model_plugins
+
+    def summary(self):
+        """Generate string representation containing the registered entrypoints."""
+        component_plugins = ", ".join(self.component_plugins)
+        model_plugins = ", ".join(self.model_plugins)
+        return (
+            f"component plugins:\n{component_plugins}"
+            f"model plugins:\n{model_plugins}"
+        )
+
+
+PLUGINS = Plugins()
---
+++ tests/test_plugins.py
@@ -0,0 +1,69 @@
+from hydromt.models.components.grid import GridComponent
+from hydromt.models.components.region import ModelRegionComponent
+from hydromt.plugins import PLUGINS
+
+
+def test_core_component_plugins():
+    components = PLUGINS.component_plugins
+    assert components == {
+        "ModelRegionComponent": ModelRegionComponent,
+        "GridComponent": GridComponent,
+    }
+    assert "ModelComponent" not in components
+
+
+# def test_plugins(mocker):
+#     ep_lst = EntryPoints(
+#         [
+#             EntryPoint(
+#                 name="test_model",
+#                 value="hydromt.models.model_api:Model",
+#                 group="hydromt.models",
+#             )
+#         ]
+#     )
+#     mocker.patch("hydromt.models.plugins._discover", return_value=ep_lst)
+#     eps = plugins.get_plugin_eps()
+#     assert "test_model" in eps
+#     assert isinstance(eps["test_model"], EntryPoint)
+
+
+# def test_plugin_duplicates(mocker):
+#     ep_lst = plugins.get_general_eps().values()
+#     mocker.patch("hydromt.models.plugins._discover", return_value=ep_lst)
+#     eps = plugins.get_plugin_eps()
+#     assert len(eps) == 0
+
+
+# def test_load():
+#     with pytest.raises(ValueError, match="Model plugin type not recognized"):
+#         plugins.load(
+#             EntryPoint(
+#                 name="erfror",
+#                 value="hydromt.data_catalog:DataCatalog",
+#                 group="hydromt.data_catalog",
+#             )
+#         )
+#     with pytest.raises(ImportError, match="Error while loading model plugin"):
+#         plugins.load(
+#             EntryPoint(
+#                 name="error", value="hydromt.models:DataCatalog", group="hydromt.models"
+#             )
+#         )
+
+
+# def test_global_models(mocker, has_xugrid):
+#     _MODELS = ModelCatalog()
+#     mocker.patch("hydromt._compat.HAS_XUGRID", has_xugrid)
+#     keys = list(plugins.LOCAL_EPS.keys())
+#     if not hydromt._compat.HAS_XUGRID:
+#         keys.remove("mesh_model")
+#     # set first local model as plugin for testing
+#     _MODELS._plugins.append(keys[0])
+#     assert isinstance(_MODELS[keys[0]], EntryPoint)
+#     assert issubclass(_MODELS.load(keys[0]), Model)
+#     assert keys[0] in _MODELS.__str__()
+#     assert all([k in _MODELS for k in keys])  # eps
+#     assert all([k in _MODELS.cls for k in keys])
+#     with pytest.raises(ValueError, match="Unknown model"):
+#         _MODELS["unknown"]
