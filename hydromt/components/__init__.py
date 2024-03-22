b9718c7fff97ef1c652ba91ae31ae9c89ed56912
Marnix Kraus <marnix.kraus@deltares.nl>
2024-03-22 14:20:24 +0100

    First try vector component

--- hydromt/components/__init__.py
+++ hydromt/components/__init__.py
@@ -4,5 +4,12 @@ from hydromt.components.base import ModelComponent
 from hydromt.components.grid import GridComponent
 from hydromt.components.region import ModelRegionComponent
 from hydromt.components.tables import TablesComponent
+from hydromt.components.vector import VectorComponent
 
-__all__ = ["ModelRegionComponent", "ModelComponent", "GridComponent", "TablesComponent"]
+__all__ = [
+    "ModelRegionComponent",
+    "ModelComponent",
+    "GridComponent",
+    "TablesComponent",
+    "VectorComponent",
+]
--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ import xarray as xr
 import xugrid as xu
 from dask import config as dask_config
 
+from hydromt.components.vector import VectorComponent
 from hydromt.data_adapter.geodataframe import GeoDataFrameAdapter
 from hydromt.data_catalog import DataCatalog
 from hydromt.driver.geodataframe_driver import GeoDataFrameDriver
@@ -325,20 +326,20 @@ def model(demda, world, obsda):
     return mod
 
 
-# @pytest.fixture()
-# def vector_model(ts, geodf):
-#     mod = VectorModel()
-#     mod.setup_config(**{"header": {"setting": "value"}})
-#     da = xr.DataArray(
-#         ts,
-#         dims=["index", "time"],
-#         coords={"index": ts.index, "time": ts.columns},
-#         name="zs",
-#     )
-#     da = da.assign_coords(geometry=(["index"], geodf["geometry"]))
-#     da.vector.set_crs(geodf.crs)
-#     mod.set_vector(da)
-#     return mod
+@pytest.fixture()
+def vector_model(ts, geodf):
+    mod = Model(components={"vector": {"type": VectorComponent.__name__}})
+    mod.setup_config(**{"header": {"setting": "value"}})
+    da = xr.DataArray(
+        ts,
+        dims=["index", "time"],
+        coords={"index": ts.index, "time": ts.columns},
+        name="zs",
+    )
+    da = da.assign_coords(geometry=(["index"], geodf["geometry"]))
+    da.vector.set_crs(geodf.crs)
+    mod.get_component("vector", VectorComponent).set(da)
+    return mod
 
 
 # @pytest.fixture()
