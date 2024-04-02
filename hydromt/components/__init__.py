e5d5fc4caee14c87d200bfde7ea9cdcd47bf5628
Sam Vente <savente93@gmail.com>
2024-04-02 09:34:11 +0200

    Table component (#850)
    Co-authored-by: hboisgon <45457510+hboisgon@users.noreply.github.com>

--- hydromt/components/__init__.py
+++ hydromt/components/__init__.py
@@ -3,9 +3,6 @@
 from hydromt.components.base import ModelComponent
 from hydromt.components.grid import GridComponent
 from hydromt.components.region import ModelRegionComponent
+from hydromt.components.tables import TablesComponent
 
-__all__ = [
-    "ModelRegionComponent",
-    "ModelComponent",
-    "GridComponent",
-]
+__all__ = ["ModelRegionComponent", "ModelComponent", "GridComponent", "TablesComponent"]
