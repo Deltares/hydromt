ebb4ffc983628876c7cb4d6ae4e9c0fb2f2671ac
Marnix Kraus <marnix.kraus@deltares.nl>
2024-04-05 09:30:58 +0200

    Rename fn to filename

--- hydromt/components/vector.py
+++ hydromt/components/vector.py
@@ -27,8 +27,8 @@ class VectorComponent(ModelComponent):
         self,
         model: Model,
         *,
-        fn: Optional[str] = "vector/vector.nc",
-        fn_geom: Optional[str] = "vector/vector.geojson",
+        filename: Optional[str] = "vector/vector.nc",
+        geometry_filename: Optional[str] = "vector/vector.geojson",
     ) -> None:
         """Initialize a vector component.
 
@@ -36,19 +36,19 @@ class VectorComponent(ModelComponent):
         ----------
         model : Model
             Parent model
-        fn : str, optional
+        filename : str, optional
             File name of the vector component, by default "vector/vector.nc".
-            If set to None, and the write/read function fn is also None, then vector will be read from/written to the geometry file only.
+            If set to None, and the write/read function filename is also None, then vector will be read from/written to the geometry file only.
             See read and write functions for more details.
-        fn_geom : str, optional
+        geometry_filename : str, optional
             File name of the vector geometry, by default "vector/vector.geojson"
-            If set to None, and the write/read function fn_geom is also None, then vector will be read from/written to the netcdf file only.
+            If set to None, and the write/read function geometry_filename is also None, then vector will be read from/written to the netcdf file only.
             See read and write functions for more details.
         """
         super().__init__(model)
         self._vector: Optional[xr.Dataset] = None
-        self.fn = fn
-        self.fn_geom = fn_geom
+        self.filename = filename
+        self.geometry_filename = geometry_filename
 
     @property
     def data(self) -> xr.Dataset:
@@ -161,30 +161,30 @@ class VectorComponent(ModelComponent):
     def read(
         self,
         *,
-        fn: Optional[str] = None,
-        fn_geom: Optional[str] = None,
+        filename: Optional[str] = None,
+        geometry_filename: Optional[str] = None,
         **kwargs,
     ) -> None:
         """Read model vector from combined netcdf and geojson file.
 
-        Files are read at <root>/<fn> and geojson file at <root>/<fn_geom>.
+        Files are read at <root>/<filename> and geojson file at <root>/<geometry_filename>.
 
         Three options are possible:
             * The netcdf file contains the attribute data and the geojson file the
                 geometry vector data.
             * The netcdf file contains both the attribute and the geometry data.
-                (fn_geom is ignored)
+                (geometry_filename is ignored)
             * The geojson file contains both the attribute and the geometry data.
-                (fn is ignored)
+                (filename is ignored)
 
         Key-word arguments are passed to :py:meth:`~hydromt.models.Model.read_nc`
 
         Parameters
         ----------
-        fn : str, optional
+        filename : str, optional
             netcdf filename relative to model root,
             by default 'vector/vector.nc'
-        fn_geom : str, optional
+        geometry_filename : str, optional
             geojson filename relative to model root,
             by default 'vector/vector.geojson'
         kwargs:
@@ -193,36 +193,40 @@ class VectorComponent(ModelComponent):
         """
         self._root._assert_read_mode()
         self._initialize_vector(skip_read=True)
-        fn = fn or self.fn
-        fn_geom = fn_geom or self.fn_geom
+        filename = filename or self.filename
+        geometry_filename = geometry_filename or self.geometry_filename
 
-        if fn is None and fn_geom is None:
-            raise ValueError("Both fn and fn_geom are None, no source file given.")
+        if filename is None and geometry_filename is None:
+            raise ValueError(
+                "Both filename and geometry_filename are None, no source file given."
+            )
 
-        if fn is not None:
+        if filename is not None:
             # Disable lazy loading of data
             # to avoid issues with reading object dtype data
             if "chunks" not in kwargs:
                 kwargs["chunks"] = None
             ds = xr.merge(
                 read_nc(
-                    fn, root=self._root.path, logger=self._logger, **kwargs
+                    filename, root=self._root.path, logger=self._logger, **kwargs
                 ).values()
             )
-            # check if ds is empty (default fn has a value)
+            # check if ds is empty (default filename has a value)
             if len(ds.sizes) == 0:
-                fn = None
-        if fn_geom is not None and isfile(join(self._root.path, fn_geom)):
-            gdf = gpd.read_file(join(self._root.path, fn_geom))
+                filename = None
+        if geometry_filename is not None and isfile(
+            join(self._root.path, geometry_filename)
+        ):
+            gdf = gpd.read_file(join(self._root.path, geometry_filename))
             # geom + netcdf data
             # TODO: What if ds is None?
-            if fn is not None:
+            if filename is not None:
                 ds = GeoDataset.from_gdf(gdf, data_vars=ds)
             # geom only
             else:
                 ds = GeoDataset.from_gdf(gdf, keep_cols=True, cols_as_data_vars=True)
         # netcdf only
-        elif fn is not None:
+        elif filename is not None:
             ds = GeoDataset.from_netcdf(ds)
         else:
             self._logger.info("No vector data found, skip reading.")
@@ -234,14 +238,14 @@ class VectorComponent(ModelComponent):
     def write(
         self,
         *,
-        fn: Optional[str] = None,
-        fn_geom: Optional[str] = None,
+        filename: Optional[str] = None,
+        geometry_filename: Optional[str] = None,
         ogr_compliant: bool = False,
         **kwargs,
-    ):
+    ) -> None:
         """Write model vector to combined netcdf and geojson files.
 
-        Files are written at <root>/<fn> and at <root>/<fn_geom> respectively.
+        Files are written at <root>/<filename> and at <root>/<geometry_filename> respectively.
 
         Three options are possible:
 
@@ -250,24 +254,24 @@ class VectorComponent(ModelComponent):
                 :py:meth:`~hydromt.models.Model.write_nc`
 
             * The netcdf file contains both the attribute and the geometry data
-                (fn_geom set to None). Key-word arguments are passed to
+                (geometry_filename set to None). Key-word arguments are passed to
                 :py:meth:`~hydromt.models.Model.write_nc`
 
             * The geojson file contains both the attribute and the geometry data
-                (fn set to None). This option is possible only if all data variables
+                (filename set to None). This option is possible only if all data variables
                 are 1D. Else the data will be written to netcdf. Key-word arguments are
                 passed to :py:meth:`~hydromt.vector.GeoDataset.to_gdf`
 
         Parameters
         ----------
-        fn : str, optional
+        filename : str, optional
             netcdf filename relative to model root,
             by default 'vector/vector.nc'
-        fn_geom : str, optional
+        geometry_filename : str, optional
             geojson filename relative to model root,
             by default 'vector/vector.geojson'
         ogr_compliant : bool
-            If fn only, write the netCDF4 file in an ogr compliant format
+            If filename only, write the netCDF4 file in an ogr compliant format
             This makes it readable as a vector file in e.g. QGIS
             see :py:meth:`~hydromt.vector.GeoBase.ogr_compliant` for more details.
         **kwargs:
@@ -279,17 +283,17 @@ class VectorComponent(ModelComponent):
             self._logger.debug("No vector data found, skip writing.")
             return
         self._root._assert_write_mode()
-        fn = fn or self.fn
-        fn_geom = fn_geom or self.fn_geom
+        filename = filename or self.filename
+        geometry_filename = geometry_filename or self.geometry_filename
 
-        if fn is None and fn_geom is None:
+        if filename is None and geometry_filename is None:
             raise ValueError(
-                "Both fn and fn_geom are None, no destination file given. Please provide either fn or fn_geom."
+                "Both filename and geometry_filename are None, no destination file given. Please provide either filename or geometry_filename."
             )
 
-        # If fn is None check if vector contains only 1D data
-        if fn is None:
-            assert fn_geom is not None
+        # If filename is None check if vector contains only 1D data
+        if filename is None:
+            assert geometry_filename is not None
             # Check if 1D data only is present
             snames = ["y_name", "x_name", "index_dim", "geom_name"]
             sdims = [ds.vector.attrs.get(n) for n in snames if n in ds.vector.attrs]
@@ -299,19 +303,20 @@ class VectorComponent(ModelComponent):
                 dims = ds[name].dims
                 # check 1D variables with matching index_dim
                 if len(dims) > 1 or dims[0] != ds.vector.index_dim:
-                    fn = join(
-                        dirname(join(self._root.path, fn_geom)),
-                        f"{basename(fn_geom).split('.')[0]}.nc",
+                    filename = join(
+                        dirname(join(self._root.path, geometry_filename)),
+                        f"{basename(geometry_filename).split('.')[0]}.nc",
                     )
                     self._logger.warning(
-                        "2D data found in vector," f"will write data to {fn} instead."
+                        "2D data found in vector,"
+                        f"will write data to {filename} instead."
                     )
                     break
 
         # write to netcdf only
-        if fn_geom is None:
-            assert fn is not None
-            os.makedirs(dirname(join(self._root.path, fn)), exist_ok=True)
+        if geometry_filename is None:
+            assert filename is not None
+            os.makedirs(dirname(join(self._root.path, filename)), exist_ok=True)
             # cannot call directly ds.vector.to_netcdf
             # because of possible PermissionError
             if ogr_compliant:
@@ -321,27 +326,31 @@ class VectorComponent(ModelComponent):
             # write_nc requires dict - use dummy key
             write_nc(
                 {"vector": ds},
-                fn,
+                filename,
                 engine="netcdf4",
                 root=self._root.path,
                 logger=self._logger,
                 **kwargs,
             )
         # write to geojson only
-        elif fn is None:
-            os.makedirs(dirname(join(self._root.path, fn_geom)), exist_ok=True)
+        elif filename is None:
+            os.makedirs(
+                dirname(join(self._root.path, geometry_filename)), exist_ok=True
+            )
             gdf = ds.vector.to_gdf(**kwargs)
-            gdf.to_file(join(self._root.path, fn_geom))
+            gdf.to_file(join(self._root.path, geometry_filename))
         # write data to netcdf and geometry to geojson
         else:
-            os.makedirs(dirname(join(self._root.path, fn_geom)), exist_ok=True)
+            os.makedirs(
+                dirname(join(self._root.path, geometry_filename)), exist_ok=True
+            )
             # write geometry
             gdf = ds.vector.geometry.to_frame("geometry")
-            gdf.to_file(join(self._root.path, fn_geom))
+            gdf.to_file(join(self._root.path, geometry_filename))
             # write_nc requires dict - use dummy key
             write_nc(
                 {"vector": ds.drop_vars("geometry")},
-                fn,
+                filename,
                 root=self._root.path,
                 logger=self._logger,
                 **kwargs,
