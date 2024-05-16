74ecc714b3743ce3bf8a42fcd499df92a919011e
Sam Vente <savente93@proton.me>
2024-05-16 09:27:09 +0200

    move example zarr file to conftest to reduce duplication

--- tests/drivers/raster/test_raster_xarray_driver.py
+++ tests/drivers/raster/test_raster_xarray_driver.py
@@ -4,9 +4,7 @@ from pathlib import Path
 from uuid import uuid4
 
 import numpy as np
-import pytest
 import xarray as xr
-import zarr
 from pytest_mock import MockerFixture
 from xarray import open_mfdataset
 
@@ -51,35 +49,6 @@ class TestRasterXarrayDriver:
         driver = RasterDatasetXarrayDriver()
         driver.write(netcdf_path, raster_ds)
         assert np.all(driver.read(str(netcdf_path)) == raster_ds)
-
-    @pytest.fixture()
-    def example_zarr_file(self, tmp_dir: Path) -> Path:
-        tmp_path: Path = tmp_dir / "0s.zarr"
-        store = zarr.DirectoryStore(tmp_path)
-        root: zarr.Group = zarr.group(store=store, overwrite=True)
-        zarray_var: zarr.Array = root.zeros(
-            "variable", shape=(10, 10), chunks=(5, 5), dtype="int8"
-        )
-        zarray_var[0, 0] = 42  # trigger write
-        zarray_var.attrs.update(
-            {
-                "_ARRAY_DIMENSIONS": ["x", "y"],
-                "coordinates": "xc yc",
-                "long_name": "Test Array",
-                "type_preferred": "int8",
-            }
-        )
-        # create symmetrical coords
-        xy = np.linspace(0, 9, 10, dtype=np.dtypes.Int8DType)
-        xcoords, ycoords = np.meshgrid(xy, xy)
-
-        zarray_x: zarr.Array = root.array("xc", xcoords, chunks=(5, 5), dtype="int8")
-        zarray_x.attrs["_ARRAY_DIMENSIONS"] = ["x", "y"]
-        zarray_y: zarr.Array = root.array("yc", ycoords, chunks=(5, 5), dtype="int8")
-        zarray_y.attrs["_ARRAY_DIMENSIONS"] = ["x", "y"]
-        zarr.consolidate_metadata(store)
-        store.close()
-        return tmp_path
 
     def test_zarr_read(self, example_zarr_file: Path):
         res: xr.Dataset = RasterDatasetXarrayDriver(
