2c441977900365beb31ca9022b9a9864bd70d237
Jaap <33715902+Jaapel@users.noreply.github.com>
2024-05-21 14:09:03 +0200

    extra allow

--- hydromt/_typing/metadata.py
+++ hydromt/_typing/metadata.py
@@ -15,9 +15,13 @@ class SourceMetadata(BaseModel):
     This refers to data that is used to enrich the data format the source is in.
     SourceMetaData is not used to reproject or fill nodata values, it is used to
     check the data and enrich the metadata for HydroMT.
+
+
+    Only the fields listed here are used in HydroMT, the rest are free for used
+    to fill in.
     """
 
-    model_config = ConfigDict(extra="forbid")
+    model_config = ConfigDict(extra="allow")
 
     crs: Union[int, str, None] = None
     unit: Optional[str] = None
@@ -25,10 +29,3 @@ class SourceMetadata(BaseModel):
     nodata: Union[dict, float, int, None] = None
     attrs: Dict[str, Any] = Field(default_factory=dict)
     category: Optional[str] = None
-    paper_doi: Optional[str] = None
-    paper_ref: Optional[str] = None
-    author: Optional[str] = None
-    license: Optional[str] = None
-    url: Optional[str] = None
-    version: Optional[Version] = None
-    notes: Optional[str] = None
