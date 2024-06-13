7a7bb604a6a88a01f13087184cacf1fd48b22e1a
Jaap <33715902+Jaapel@users.noreply.github.com>
2024-06-13 16:32:32 +0200

    even shorter

--- tests/data_catalog/sources/test_common_functionality.py
+++ tests/data_catalog/sources/test_common_functionality.py
@@ -1,12 +1,13 @@
-from typing import Any, Dict, Tuple, Type
+from typing import Type
 
 import pytest
 from pydantic import ValidationError
 
-from hydromt.data_catalog.adapters import DataFrameAdapter, DatasetAdapter
+from hydromt.data_catalog.adapters.data_adapter_base import DataAdapterBase
 from hydromt.data_catalog.sources import (
     DataFrameSource,
     DatasetSource,
+    DataSource,
     GeoDataFrameSource,
     GeoDatasetSource,
     RasterDatasetSource,
@@ -14,92 +15,30 @@ from hydromt.data_catalog.sources import (
 
 
 class TestValidators:
-    @pytest.fixture()
-    def dataframe_source_args(
-        self, mock_df_adapter: DataFrameAdapter
-    ) -> Tuple[Type[DataFrameSource], Dict[str, Any]]:
-        return (
-            DataFrameSource,
-            dict(
-                name="name",
-                uri="uri",
-                data_adapter=mock_df_adapter,
-                driver="does not exist",
-            ),
-        )
-
-    @pytest.fixture()
-    def dataset_source_args(
-        self, mock_ds_adapter: DatasetAdapter
-    ) -> Tuple[Type[DatasetSource], Dict[str, Any]]:
-        return (
-            DatasetSource,
-            dict(
-                name="name",
-                uri="uri",
-                data_adapter=mock_ds_adapter,
-                driver="does not exist",
-            ),
-        )
-
-    @pytest.fixture()
-    def geodataframe_source_args(
-        self, mock_geodataframe_adapter
-    ) -> Tuple[Type[GeoDataFrameSource], Dict[str, Any]]:
-        return (
-            GeoDataFrameSource,
-            dict(
-                name="name",
-                uri="uri",
-                data_adapter=mock_geodataframe_adapter,
-                driver="does not exist",
-            ),
-        )
-
-    @pytest.fixture()
-    def geodataset_source_args(
-        self, mock_geo_ds_adapter
-    ) -> Tuple[Type[GeoDatasetSource], Dict[str, Any]]:
-        return (
-            GeoDatasetSource,
-            dict(
-                name="name",
-                uri="uri",
-                data_adapter=mock_geo_ds_adapter,
-                driver="does not exist",
-            ),
-        )
-
-    @pytest.fixture()
-    def rasterdataset_source_args(
-        self, mock_raster_ds_adapter
-    ) -> Tuple[Type[RasterDatasetSource], Dict[str, Any]]:
-        return (
-            RasterDatasetSource,
-            dict(
-                name="name",
-                uri="uri",
-                data_adapter=mock_raster_ds_adapter,
-                driver="does not exist",
-            ),
-        )
-
-    fixture_sources = pytest.mark.parametrize(
-        "source",
+    @pytest.mark.parametrize(
+        "source_cls,_adapter",  # noqa: PT006
         [
-            "dataframe_source_args",
-            "dataset_source_args",
-            "geodataframe_source_args",
-            "geodataset_source_args",
-            "rasterdataset_source_args",
+            (DataFrameSource, "mock_df_adapter"),
+            (DatasetSource, "mock_ds_adapter"),
+            (GeoDataFrameSource, "mock_geodataframe_adapter"),
+            (GeoDatasetSource, "mock_geo_ds_adapter"),
+            (RasterDatasetSource, "mock_raster_ds_adapter"),
         ],
     )
-
-    @fixture_sources
-    def test_validators(self, source: str, request: pytest.FixtureRequest):
-        source_cls, source_vals = request.getfixturevalue(source)
+    def test_validators(
+        self,
+        source_cls: Type[DataSource],
+        _adapter: str,  # noqa: PT019
+        request: pytest.FixtureRequest,
+    ):
+        adapter: DataAdapterBase = request.getfixturevalue(_adapter)
         with pytest.raises(ValidationError) as e_info:
-            source_cls(**source_vals)
+            source_cls(
+                name="name",
+                uri="uri",
+                data_adapter=adapter,
+                driver="does_not_exist",
+            )
 
         assert e_info.value.error_count() == 1
         error_driver = next(
