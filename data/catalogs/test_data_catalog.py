2125b959b1ecf5a223e887e1923e749e237eafb3
Tjalling-dejong <tjalling.dejong@deltares.nl>
2024-06-21 12:55:29 +0200

    updated test_data_catalog.py

--- data/catalogs/test_data_catalog.py
+++ data/catalogs/test_data_catalog.py
@@ -29,6 +29,9 @@ from pydantic_core import ValidationError
 
 from hydromt import DataCatalog
 from hydromt._validators.data_catalog import DataCatalogValidator
+from hydromt.data_catalog.uri_resolvers.raster_tindex_resolver import (
+    RasterTindexResolver,
+)
 from hydromt.utils import setuplog
 
 
@@ -70,20 +73,27 @@ def test_data_catalog(args, datacatalog):
     """Tests the paths of the given data catalog."""
     error_count = 0
     logger.info("Checking paths of data catalog sources")
-    for source_name in datacatalog.get_source_names():
-        try:
-            logger.info(f"Checking paths of {source_name}")
-            if not exists(datacatalog.get_source(source_name).uri):
-                raise FileNotFoundError
-        except FileNotFoundError as e:
-            logger.error(f"File not found for dataset source {source_name}: {e}")
+    for source_name, source in datacatalog.__iter__():
+        logger.info(f"Checking paths of {source_name}")
+        if isinstance(source.driver.metadata_resolver, RasterTindexResolver):
+            if not exists(source.full_uri):
+                error_count += 1
+                logger.error(
+                    f"File {source.full_uri} not found for dataset source {source_name}"
+                )
+            continue
 
-            error_count += 1
-        except ValueError as e:
-            logger.error(
-                f"Something went wrong with creating path string for dataset source {source_name}: {e}"
+        else:
+            paths = source.driver.metadata_resolver.resolve(
+                source.full_uri, source.driver.filesystem
             )
-            error_count += 1
+            for path in paths:
+                if not exists(path):
+                    logger.error(
+                        f"File {path} not found for dataset source {source_name}"
+                    )
+                    error_count += 1
+
     if error_count > 0:
         logger.error(f"Encountered {error_count} errors")
 
