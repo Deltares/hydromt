6f98e6f77d4b2c1bdeb94cba9666524ffaea84be
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-10-21 22:46:23 +0200

    force testbank to run

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -60,7 +60,7 @@ def _patch_plugin_entry_point(
     mock_single_entrypoint = mocker.create_autospec(EntryPoint, spec_set=True)
     mock_single_entrypoint.dist.version = "999.9.9"
     mock_single_entrypoint.dist.name = "hydromt-test"
-    # mock_single_entrypoint.name = "test" # this cannot be mocked?
+    # mock_single_entrypoint.name = "test" # can this not be mocked?
 
     mock_multiple_entrypoints = mocker.create_autospec(EntryPoints, spec_set=True)
     mock_multiple_entrypoints.__iter__.return_value = [mock_single_entrypoint]
