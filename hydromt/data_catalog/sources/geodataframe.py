9924ff1d8977be9315fda481ae1cafae9bfc6ec0
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-09-08 11:16:01 +0200

    improved test coverage

--- hydromt/data_catalog/sources/geodataframe.py
+++ hydromt/data_catalog/sources/geodataframe.py
@@ -237,7 +237,7 @@ class GeoDataFrameSource(DataSource):
                 media_type = MediaType.GEOPACKAGE
             else:
                 raise RuntimeError(
-                    f"Unknown extension: {ext} cannot determine media type"
+                    f"Unknown extension: {ext}, cannot determine media type"
                 )
         except (IndexError, KeyError, CRSError, TypeError) as e:
             if handle_nodata == NoDataStrategy.IGNORE:
