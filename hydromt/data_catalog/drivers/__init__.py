739ce20c74599aecdc279d1201cc26073996db77
Luuk Blom <luuk.blom@deltares.nl>
2025-10-10 09:45:36 +0200

    fix imports in `__init__.py`

--- hydromt/data_catalog/drivers/__init__.py
+++ hydromt/data_catalog/drivers/__init__.py
@@ -1,18 +1,23 @@
 """Drivers responsible for reading and writing data."""
 
-from hydromt.data_catalog.drivers.base_driver import BaseDriver
+from hydromt.data_catalog.drivers.base_driver import BaseDriver, DriverOptions
 from hydromt.data_catalog.drivers.dataframe import DataFrameDriver, PandasDriver
 from hydromt.data_catalog.drivers.dataset import DatasetDriver, DatasetXarrayDriver
+from hydromt.data_catalog.drivers.dataset.xarray_driver import DatasetXarrayOptions
 from hydromt.data_catalog.drivers.geodataframe import (
     GeoDataFrameDriver,
     GeoDataFrameTableDriver,
     PyogrioDriver,
+)
+from hydromt.data_catalog.drivers.geodataframe.table_driver import (
+    GeoDataFrameTableOptions,
 )
 from hydromt.data_catalog.drivers.geodataset import (
     GeoDatasetDriver,
     GeoDatasetVectorDriver,
     GeoDatasetXarrayDriver,
 )
+from hydromt.data_catalog.drivers.geodataset.geodataset_driver import GeoDatasetOptions
 from hydromt.data_catalog.drivers.preprocessing import (
     harmonise_dims,
     remove_duplicates,
