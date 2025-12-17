dacf7549d7bff33c22de3f02229e24af3143d66f
Luuk Blom <luuk.blom@deltares.nl>
2025-12-17 13:24:05 +0100

    fix dict is not hashable error

--- hydromt/readers.py
+++ hydromt/readers.py
@@ -215,6 +215,11 @@ def open_raster(
     if isinstance(uri, IOBase):  # file-like does not handle chunks
         logger.warning("Removing chunks to read and load remote data.")
         kwargs.pop("chunks")
+
+    for k, v in kwargs.items():
+        if isinstance(v, dict):
+            kwargs[k] = tuple(v)  # dicts are not hashable, so convert to tuple
+
     # keep only 2D DataArray
     da: xr.DataArray = rioxarray.open_rasterio(uri, **kwargs).squeeze(drop=True)
     # set missing _FillValue
