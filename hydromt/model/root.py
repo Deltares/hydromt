519b62e5132ad00f7482126b3777757f7bd26102
dalmijn <brencodeert@outlook.com>
2026-03-09 14:07:21 +0100

    Fix root and rename model mode file

--- hydromt/model/root.py
+++ hydromt/model/root.py
@@ -4,7 +4,7 @@ import logging
 from pathlib import Path
 from typing import Optional
 
-from hydromt.typing import ModeLike, ModelMode
+from hydromt.model.mode import ModelMode
 
 logger = logging.getLogger(__name__)
 
@@ -12,35 +12,32 @@ __all__ = ["ModelRoot"]
 
 
 class ModelRoot:
-    """A class to handle model roots in a cross platform manner."""
+    """Handle model roots in a cross platform manner.
+
+    Parameters
+    ----------
+    path : Path
+        The path to the root of the model.
+    mode : ModelMode | str, optional
+        The mode of the model (e.g. 'r' for reading, 'w' for writing etc.),
+        by default "w".
+    """
 
     def __init__(
         self,
         path: Path,
-        mode: ModeLike = "w",
+        mode: ModelMode | str = "w",
     ):
-        self._mode = ModelMode.from_str_or_mode(mode)
-        if self.is_reading_mode():
-            self._check_root_exists(path)
-        self.path = Path(path).resolve()
-        self.path.mkdir(parents=True, exist_ok=True)
+        self._mode: ModelMode | None = None
+        self._path: Path | None = None
 
-    def set(
-        self,
-        path: Path,
-        mode: Optional[ModeLike] = None,
-    ) -> Path:
-        """Set the path and mode of the root."""
-        if mode:
-            self.mode = mode
+        # Set the Root
+        self.set(path=path, mode=mode)
 
-        if self.is_reading_mode():
-            self._check_root_exists(path)
+    def __repr__(self):
+        return f"ModelRoot(path={self.path}, mode={self._mode})"
 
-        self.path = Path(path).resolve()
-
-        return self.path
-
+    ## Private methods
     def _assert_write_mode(self) -> None:
         if not self.mode.is_writing_mode():
             raise IOError("Model opened in read-only mode")
@@ -49,33 +46,74 @@ class ModelRoot:
         if not self.mode.is_reading_mode():
             raise IOError("Model opened in write-only mode")
 
+    def _check_root_exists(self, path: Optional[Path] = None) -> None:
+        path = path or self.path
+        if not path.exists():
+            raise IOError(f'model root not found at "{path}"')
+
+    ## Properties
     @property
     def mode(self) -> ModelMode:
         """The mode of the model this object belongs to."""
         return self._mode
 
     @mode.setter
-    def mode(self, mode: ModeLike) -> ModelMode:
+    def mode(self, mode: ModelMode | str) -> ModelMode:
         """Set the mode of the model."""
         self._mode: ModelMode = ModelMode.from_str_or_mode(mode)
         return self._mode
+
+    @property
+    def path(self) -> Path:
+        """Return the path of the model root."""
+        return self._path
+
+    @path.setter
+    def path(self, path: Path | str):
+        """Set the path of the model."""
+        path = Path(path).resolve()
+        if self.is_reading_mode():
+            self._check_root_exists(path)
+        if self.is_writing_mode():
+            path.mkdir(parents=True, exist_ok=True)
+        self._path = path
+
+    ## Checks
+    def is_reading_mode(self) -> bool:
+        """Test whether we are in reading mode or not."""
+        return self._mode.is_reading_mode()
 
     def is_writing_mode(self) -> bool:
         """Test whether we are in writing mode or not."""
         return self._mode.is_writing_mode()
 
-    def is_reading_mode(self) -> bool:
-        """Test whether we are in reading mode or not."""
-        return self._mode.is_reading_mode()
-
     def is_override_mode(self) -> bool:
         """Test whether we are in override mode or not."""
         return self._mode.is_override_mode()
 
-    def __repr__(self):
-        return f"ModelRoot(path={self.path}, mode={self._mode})"
+    ## Mutating methods
+    def set(
+        self,
+        path: Path,
+        mode: ModelMode | str | None = None,
+    ) -> Path:
+        """Set a new model root and mode.
 
-    def _check_root_exists(self, path: Optional[Path] = None) -> None:
-        path = path or self.path
-        if not path.exists():
-            raise IOError(f'model root not found at "{path}"')
+        Parameters
+        ----------
+        path : Path
+            The path to the root of the model.
+        mode : ModelMode | str | None, optional
+            The mode of the model. If not provided, the mode currently defined in the
+            `ModelRoot` is used. By default None.
+
+        Returns
+        -------
+        Path
+            The path to the new model root.
+        """
+        if mode is not None:
+            self.mode = mode
+        self.path = path
+
+        return self.path
--- tests/model/test_mode.py
+++ tests/model/test_mode.py
@@ -2,7 +2,7 @@ from os.path import abspath
 
 import pytest
 
-from hydromt.typing import ModelMode
+from hydromt.model.mode import ModelMode
 
 # we need to compensate for where the repo is located when
 # we run the tests
